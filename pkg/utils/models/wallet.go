cd30babe8b8d6c059617ad5f9b823c3099855a14
Anandhu4456 <anandhurajan56@gmail.com>
2023-10-22 23:53:51 +0530

    completed wallet model wishlist

--- pkg/utils/models/wallet.go
+++ pkg/utils/models/wallet.go
@@ -1,1 +1,8 @@
 package models
+
+import "github.com/Anandhu4456/go-Ecommerce/pkg/domain"
+
+type Wallet struct {
+	Balance int                    `json:"balance"`
+	History []domain.WalletHistory `json:"history"`
+}
--- pkg/utils/models/wishlis.go
+++ pkg/utils/models/wishlis.go
@@ -1,1 +1,7 @@
 package models
+
+type GetWishlist struct {
+	ProductName string  `json:"product_name"`
+	Category    string  `json:"category"`
+	Price       float64 `json:"price"`
+}
