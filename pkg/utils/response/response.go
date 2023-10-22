f991bde94ee798b87587e6110a54a6da99ccc7bd
Anandhu4456 <anandhurajan56@gmail.com>
2023-10-23 00:00:46 +0530

    client response model created

--- pkg/utils/response/response.go
+++ pkg/utils/response/response.go
@@ -1,1 +1,17 @@
 package response
+
+type Response struct {
+	StatusCode int         `json:"statuscode"`
+	Message    string      `json:"message"`
+	Data       interface{} `json:"data"`
+	Error      interface{} `json:"error"`
+}
+
+func ClientResponse(statusCode int, message string, data interface{}, err interface{}) Response {
+	return Response{
+		StatusCode: statusCode,
+		Message:    message,
+		Data:       data,
+		Error:      err,
+	}
+}
