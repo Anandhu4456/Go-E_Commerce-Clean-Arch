747e05c0fe45cd257a4c3033fffab402da5a6426
Anandhu4456 <anandhurajan56@gmail.com>
2023-10-23 00:38:16 +0530

    completed admin domain and cart domain

--- pkg/domain/admin.go
+++ pkg/domain/admin.go
@@ -1,1 +1,8 @@
 package domain
+
+type Admin struct {
+	ID       uint   `json:"id" gorm:"unique;not null"`
+	Username string `json:"username" gorm:"validate:required"`
+	Email    string `json:"email" gorm:"validate:required"`
+	Password string `json:"password" gorm:"validate:required"`
+}
--- pkg/domain/cart.go
+++ pkg/domain/cart.go
@@ -1,1 +1,18 @@
 package domain
+
+// cart of a user
+type Cart struct {
+	ID     uint `json:"id" gorm:"primarykey"`
+	UserID uint `json:"user_id" gorm:"not null"`
+	User   User `json:"user" gorm:"foreignkey:UserID"`
+}
+
+// products in the cart of user
+type LineItems struct {
+	ID          uint      `json:"id" gorm:"primarykey"`
+	CartID      uint      `json:"cart_id" gorm:"not null"`
+	Cart        Cart      `json:"-" gorm:"foreignkey:CartID"`
+	InventoryID uint      `json:"inventory_id" gorm:"not null"`
+	Inventory   Inventory `json:"-" gorm:"foreignkey:InventoryID"`
+	Quantity    int       `json:"quantity" gorm:"default:1"`
+}
