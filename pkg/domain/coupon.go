52421d8ea0f0320746637f3b6289828036c3843a
Anandhu4456 <anandhurajan56@gmail.com>
2023-10-23 19:23:32 +0530

    completed coupon domain and inventory and deleted the category file that not necessery

--- pkg/domain/coupon.go
+++ pkg/domain/coupon.go
@@ -1,1 +1,8 @@
 package domain
+
+type Coupon struct {
+	ID int `json:"id" gorm:"primarykey"`
+	Name string `json:"name" gorm:"unique;not null"`
+	DiscountRate int `json:"discount_rate"`
+	Valid bool `gorm:"default:true"`
+}
--- pkg/domain/inventory.go
+++ pkg/domain/inventory.go
@@ -1,1 +1,26 @@
 package domain
+
+// products in the website
+type Inventory struct {
+	ID uint `json:"id" gorm:"unique;not null"`
+	CategoryID int `json:"category_id"`
+	Category Category `json:"-" gorm:"foreignkey:CategoryID;constraint:OnDelete:CASCADE"`
+	ProductName string `json:"product_name"`
+	Description string `json:"description"`
+	Image string `json:"image"`
+	Stock int `json:"stock"`
+	Price float64 `json:"price"`
+}
+
+// category of product
+type Category struct{
+	ID int `json:"id" gorm:"primarykey;not null"`
+	Category string `json:"category"`
+}
+
+type Image struct{
+	ID int`json:"id" gorm:"primarykey;not null"`
+	InventoryID int `json:"inventory_id"`
+	Inventory Inventory `json:"-" gorm:"foreignkey:InventoryID"`
+	ImageUrl string `json:"image_url"`
+}
