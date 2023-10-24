6eb81f5ff5113d23d6c609a17442b1d87876db0c
Anandhu4456 <anandhurajan56@gmail.com>
2023-10-24 23:31:41 +0530

    category interface created in repo

--- pkg/repository/interfaces/category.go
+++ pkg/repository/interfaces/category.go
@@ -1,1 +1,11 @@
 package interfaces
+
+import "github.com/Anandhu4456/go-Ecommerce/pkg/domain"
+
+type CategoryRepository interface{
+	AddCategory(category string)(domain.Category,error)
+	CheckCategory(current string)(bool,error)
+	UpdateCategory(current string)(domain.Category,error)
+	DeleteCategory(categoryId string)error
+	GetCategories(page,limit int)([]domain.Category,error)
+}
