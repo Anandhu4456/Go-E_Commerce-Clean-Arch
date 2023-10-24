6d816696a26e19e52aa609a94dc96016c61f3d75
Anandhu4456 <anandhurajan56@gmail.com>
2023-10-24 14:13:48 +0530

    db connection established using gorm

--- go.mod
+++ go.mod
@@ -5,11 +5,18 @@ go 1.18
 require (
 	github.com/joho/godotenv v1.5.1
 	github.com/spf13/viper v1.17.0
+	gorm.io/driver/postgres v1.5.3
+	gorm.io/gorm v1.25.5
 )
 
 require (
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.4.3 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
@@ -22,6 +29,7 @@ require (
 	github.com/subosito/gotenv v1.6.0 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.9.0 // indirect
+	golang.org/x/crypto v0.13.0 // indirect
 	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
 	golang.org/x/sys v0.12.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
--- pkg/db/connection.go
+++ pkg/db/connection.go
@@ -1,1 +1,38 @@
 package db
+
+import (
+	"fmt"
+
+	"github.com/Anandhu4456/go-Ecommerce/pkg/config"
+	"github.com/Anandhu4456/go-Ecommerce/pkg/domain"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func ConnectDB(cfg config.Config) (*gorm.DB, error) {
+	psqlInfo := fmt.Sprintf("host=%s user=%s dbpassword=%s dbport=%s dbname=%s", cfg.DBHost, cfg.DBUser, cfg.DBPassword, cfg.DBPort, cfg.DBName)
+
+	db, dbErr := gorm.Open(postgres.Open(psqlInfo), &gorm.Config{
+		SkipDefaultTransaction: true,
+	})
+
+	db.AutoMigrate(&domain.Inventory{})
+	db.AutoMigrate(&domain.Category{})
+	db.AutoMigrate(&domain.Admin{})
+	db.AutoMigrate(&domain.User{})
+	db.AutoMigrate(&domain.Cart{})
+	db.AutoMigrate(&domain.WishList{})
+	db.AutoMigrate(&domain.WishlistItems{})
+	db.AutoMigrate(&domain.Address{})
+	db.AutoMigrate(&domain.Order{})
+	db.AutoMigrate(&domain.OrderItem{})
+	db.AutoMigrate(&domain.LineItems{})
+	db.AutoMigrate(&domain.PaymentMethod{})
+	db.AutoMigrate(&domain.Offer{})
+	db.AutoMigrate(&domain.Coupon{})
+	db.AutoMigrate(&domain.Wallet{})
+	db.AutoMigrate(&domain.WalletHistory{})
+	db.AutoMigrate(&domain.Image{})
+
+	return db, dbErr
+}
