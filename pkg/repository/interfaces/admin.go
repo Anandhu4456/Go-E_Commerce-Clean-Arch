12f99c1f04f154c60859394b75fa02d36d880508
Anandhu4456 <anandhurajan56@gmail.com>
2023-10-24 19:00:13 +0530

    admin interface created in repo

--- pkg/repository/interfaces/admin.go
+++ pkg/repository/interfaces/admin.go
@@ -1,1 +1,13 @@
 package interfaces
+
+import (
+	"github.com/Anandhu4456/go-Ecommerce/pkg/domain"
+	"github.com/Anandhu4456/go-Ecommerce/pkg/utils/models"
+)
+
+type AdminRepository interface {
+	LoginHandler(adminDetails models.AdminLogin) (domain.Admin, error)
+	GetUserById(id string) (domain.User, error)
+	UpdateBlockUserById(user domain.User) error
+	GetUsers(page, limit int) ([]models.UserDetailsAtAdmin, error)
+}
