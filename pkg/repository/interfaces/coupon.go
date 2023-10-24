b646a9279dfabf8ff39270e66cbc2125c95f05fb
Anandhu4456 <anandhurajan56@gmail.com>
2023-10-24 23:39:33 +0530

    coupon interface created in repo

--- pkg/repository/interfaces/coupon.go
+++ pkg/repository/interfaces/coupon.go
@@ -1,1 +1,11 @@
 package interfaces
+
+import "github.com/Anandhu4456/go-Ecommerce/pkg/domain"
+
+type CouponRepository interface{
+	AddCoupon(domain.Coupon)error
+	MakeCouponInvalid(id int)error
+	FindCouponDiscount(coupon string)int
+	GetCoupons(page,limit int)([]domain.Coupon,error)
+	ValidateCoupon(coupon string)(bool,error)
+}
