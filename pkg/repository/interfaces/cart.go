d1af42f16871eef801d508dae29c425c4be55734
Anandhu4456 <anandhurajan56@gmail.com>
2023-10-24 19:38:57 +0530

    created cart repo interface

--- pkg/repository/interfaces/cart.go
+++ pkg/repository/interfaces/cart.go
@@ -1,1 +1,12 @@
 package interfaces
+
+import "github.com/Anandhu4456/go-Ecommerce/pkg/domain"
+
+type CartRepository interface {
+	GetAddresses(id int) ([]domain.Address, error)
+	CheckIfInvAdded(invId, cartId int) bool
+	GetCartId(user_id int) (int, error)
+	CreateNewCart(user_id int) (int, error)
+	AddLineItems(invId, cartId int) error
+	AddQuantity(invId, cartId int) error
+}
