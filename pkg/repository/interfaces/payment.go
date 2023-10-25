ccda3a3890bb33f89e022b8a5a151556fdc14f8c
Anandhu4456 <anandhurajan56@gmail.com>
2023-10-25 20:09:00 +0530

    payment interface in repo

--- pkg/repository/interfaces/payment.go
+++ pkg/repository/interfaces/payment.go
@@ -1,1 +1,12 @@
 package interfaces
+
+import "github.com/Anandhu4456/go-Ecommerce/pkg/domain"
+
+type PaymentRepository interface{
+	AddNewPaymentMethod(paymentMethod string)error
+	RemovePaymentMethod(paymentMethodId int)error
+	GetPaymentMethods()([]domain.PaymentMethod,error)
+	FindUsername(user_id int)(string,error)
+	FindPrice(order_id int)(float64,error)
+	UpdatePaymentDetails(orderId,paymentId,razorId string)error
+}
