fa2a1352b797231a0adec631a5c5b707f3a7030d
Anandhu4456 <anandhurajan56@gmail.com>
2023-10-25 19:44:27 +0530

    offer interface created in repo

--- pkg/repository/interfaces/offer.go
+++ pkg/repository/interfaces/offer.go
@@ -1,1 +1,13 @@
 package interfaces
+
+import (
+	"github.com/Anandhu4456/go-Ecommerce/pkg/domain"
+	"github.com/Anandhu4456/go-Ecommerce/pkg/utils/models"
+)
+
+type OfferRepository interface {
+	AddNewOffer(models.CreateOffer) error
+	MakeOfferExpired(cartId int) error
+	FindDiscountPercentage(cartId int) (int, error)
+	GetOffers(page, limit int) ([]domain.Offer, error)
+}
