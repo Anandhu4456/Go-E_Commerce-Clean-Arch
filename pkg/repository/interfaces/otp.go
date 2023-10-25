f63f5e9739901341835101f2d738a5ae7c9a3aee
Anandhu4456 <anandhurajan56@gmail.com>
2023-10-25 20:02:52 +0530

    otp interface in repo

--- pkg/repository/interfaces/otp.go
+++ pkg/repository/interfaces/otp.go
@@ -1,1 +1,8 @@
 package interfaces
+
+import "github.com/Anandhu4456/go-Ecommerce/pkg/utils/models"
+
+type OtpRepository interface {
+	FindUserByMobileNumber(phone string)bool
+	UserDetailsUsingPhone(phone string)(models.UserResponse,error)
+}
