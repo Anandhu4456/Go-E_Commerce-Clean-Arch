f2330babfb4b5009d14f20100cb5a14f7f2738a1
Anandhu4456 <anandhurajan56@gmail.com>
2023-10-25 23:11:43 +0530

    created wishlist interface in repo

--- pkg/repository/interfaces/wishlist.go
+++ pkg/repository/interfaces/wishlist.go
@@ -1,1 +1,19 @@
 package interfaces
+
+import (
+	
+	"github.com/Anandhu4456/go-Ecommerce/pkg/utils/models"
+)
+
+
+type WishlistRepository interface{
+	GetWishlist(id int)([]models.GetWishlist,error)
+	GetWishlistId(user_id int)(int, error)
+	CreateNewWishlist(user_id int)(int,error)
+	AddWishlistItem(wishlistId,inventoryId int)error
+	GetProductsInWishlist(wishlistId int)([]int,error)
+	FindProductNames(inventory_id int)(string,error)
+	FindPrice(inventory_id int)(float64,error)
+	FindCategory(inventory_id int)(string,error)
+	RemoveFromWishlist(wishlistId,inventoryId int)error
+}
