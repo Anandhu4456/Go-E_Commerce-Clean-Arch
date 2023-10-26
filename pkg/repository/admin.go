375a3d7e10fc5be327127b91374b46ce80f2f99e
Anandhu4456 <anandhurajan56@gmail.com>
2023-10-26 19:46:11 +0530

    created loginhandler function in repo

--- pkg/repository/admin.go
+++ pkg/repository/admin.go
@@ -1,1 +1,20 @@
 package repository
+
+import (
+	"github.com/Anandhu4456/go-Ecommerce/pkg/domain"
+	"github.com/Anandhu4456/go-Ecommerce/pkg/utils/models"
+	"gorm.io/gorm"
+)
+
+type adminRepository struct {
+	DB *gorm.DB
+}
+
+func (ar *adminRepository) LoginHandler(adminDetails models.AdminLogin) (domain.Admin, error) {
+	var adminCompareDetail domain.Admin
+	err := ar.DB.Raw("select * from admins where email=?", adminDetails.Email).Scan(&adminCompareDetail).Error
+	if err != nil {
+		return domain.Admin{}, err
+	}
+	return adminCompareDetail, nil
+}
