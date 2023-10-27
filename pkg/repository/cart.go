66863bd6087fcaa224f8c1e2f3228d871390e1a2
Anandhu4456 <anandhurajan56@gmail.com>
2023-10-27 13:07:33 +0530

    created get addresses and getcart id in cart repo

--- pkg/repository/cart.go
+++ pkg/repository/cart.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	"github.com/Anandhu4456/go-Ecommerce/pkg/domain"
 	"github.com/Anandhu4456/go-Ecommerce/pkg/repository/interfaces"
 	"gorm.io/gorm"
 )
@@ -16,3 +17,20 @@ func NewCartRepository (DB *gorm.DB)interfaces.CartRepository{
 	}
 }
 
+func (cr *cartRepository)GetAddresses(id int)([]domain.Address,error){
+	var addresses []domain.Address
+	err:=cr.DB.Raw("select * from addresses where id = ?",id).Scan(&addresses).Error
+	if err!=nil{
+		return []domain.Address{},err
+	}
+	return addresses,nil
+}
+
+func (cr *cartRepository)GetCartId(user_id int)(int,error){
+	var userId int
+	err:=cr.DB.Raw("select id from carts where user_id = ?",userId).Scan(&userId).Error
+	if err!=nil{
+		return 0,err
+	}
+	return userId,nil
+}
