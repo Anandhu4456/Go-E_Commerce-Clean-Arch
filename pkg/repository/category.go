c13e6e92d21bd44c7905135fa7f098d0b203670d
Anandhu4456 <anandhurajan56@gmail.com>
2023-10-28 23:47:38 +0530

    add category function created in category repo

--- pkg/repository/category.go
+++ pkg/repository/category.go
@@ -1,16 +1,41 @@
 package repository
 
 import (
+	"github.com/Anandhu4456/go-Ecommerce/pkg/domain"
 	"github.com/Anandhu4456/go-Ecommerce/pkg/repository/interfaces"
 	"gorm.io/gorm"
 )
 
-type categoryRepository struct{
+type categoryRepository struct {
 	DB *gorm.DB
 }
 
-func NewCategoryRepository(db *gorm.DB)interfaces.CategoryRepository{
+func NewCategoryRepository(db *gorm.DB) interfaces.CategoryRepository {
 	return &categoryRepository{
-		DB:db,
+		DB: db,
 	}
+}
+
+func (cat *categoryRepository) AddCategory(category string) (domain.Category, error) {
+	var b string
+	err := cat.DB.Raw("INSERT INTO categories(category)VALUES(?)RETURNING category", category).Scan(&b).Error
+	if err != nil {
+		return domain.Category{}, err
+	}
+	var categoryResponse domain.Category
+	err = cat.DB.Raw(`
+		SELECT 
+
+		cat.id,
+		cat.category
+		
+		FROM
+		categories cat
+		WHERE
+		cat.category = ?
+	`, b).Scan(&categoryResponse).Error
+	if err != nil {
+		return domain.Category{}, err
+	}
+	return categoryResponse, nil
 }
