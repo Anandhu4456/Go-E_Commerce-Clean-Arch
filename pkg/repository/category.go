27f8d05ad2606a049de3cfc9f07843c6c6b30068
Anandhu4456 <anandhurajan56@gmail.com>
2023-10-28 23:34:17 +0530

    created a construction function for category repository

--- pkg/repository/category.go
+++ pkg/repository/category.go
@@ -1,1 +1,16 @@
 package repository
+
+import (
+	"github.com/Anandhu4456/go-Ecommerce/pkg/repository/interfaces"
+	"gorm.io/gorm"
+)
+
+type categoryRepository struct{
+	DB *gorm.DB
+}
+
+func NewCategoryRepository(db *gorm.DB)interfaces.CategoryRepository{
+	return &categoryRepository{
+		DB:db,
+	}
+}
