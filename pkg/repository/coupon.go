8ea563679086f74be43357066976f43c2f367cdb
Anandhu4456 <anandhurajan56@gmail.com>
2023-10-29 21:59:31 +0530

    created a construction function for coupon repository

--- pkg/repository/coupon.go
+++ pkg/repository/coupon.go
@@ -1,1 +1,18 @@
 package repository
+
+import (
+	"github.com/Anandhu4456/go-Ecommerce/pkg/repository/interfaces"
+	"gorm.io/gorm"
+)
+
+
+type couponRepository struct{
+	DB *gorm.DB
+}
+
+// constructor function
+func NewCouponRepository(DB *gorm.DB)interfaces.CouponRepository{
+	return &couponRepository{
+		DB:DB,
+	}
+}
