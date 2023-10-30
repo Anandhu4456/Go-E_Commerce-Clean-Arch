60fd4a91fb0a2eedbb0b987a76ec71e5910ef363
Anandhu4456 <anandhurajan56@gmail.com>
2023-10-30 18:08:19 +0530

    created a constructor function for inventory repository

--- pkg/repository/inventory.go
+++ pkg/repository/inventory.go
@@ -1,1 +1,18 @@
 package repository
+
+import (
+	"github.com/Anandhu4456/go-Ecommerce/pkg/repository/interfaces"
+	"gorm.io/gorm"
+)
+
+type inventoryRespository struct {
+	DB *gorm.DB
+}
+
+// Constructor function
+
+func NewInventoryRepository(DB *gorm.DB) interfaces.InventoryRespository {
+	return &inventoryRespository{
+		DB: DB,
+	}
+}
