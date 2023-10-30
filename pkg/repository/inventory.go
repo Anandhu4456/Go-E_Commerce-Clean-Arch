8ddea92c2741829d44812ab24c0393f5e48b2b50
Anandhu4456 <anandhurajan56@gmail.com>
2023-10-30 18:35:39 +0530

    created update image functon in inventory repo

--- pkg/repository/inventory.go
+++ pkg/repository/inventory.go
@@ -1,6 +1,8 @@
 package repository
 
 import (
+	"errors"
+
 	"github.com/Anandhu4456/go-Ecommerce/pkg/repository/interfaces"
 	"github.com/Anandhu4456/go-Ecommerce/pkg/utils/models"
 	"gorm.io/gorm"
@@ -29,4 +31,26 @@ func (ir *inventoryRespository) AddInventory(inventory models.Inventory, url str
 		return models.InventoryResponse{}, err
 	}
 	return models.InventoryResponse{}, nil
+}
+
+func (ir *inventoryRespository) UpdateImage(inventId int, url string) (models.Inventory, error) {
+
+	// check db connecton
+	if ir.DB == nil {
+		return models.Inventory{}, errors.New("database connection failed while updating image")
+	}
+
+	// updating image
+	err := ir.DB.Exec("UPDATE inventories SET image=? WHERE id= ? ", url, inventId).Error
+
+	if err != nil {
+		return models.Inventory{}, err
+	}
+	// Retrieve the update
+	var updatedImageInventory models.Inventory
+	err = ir.DB.Raw("SELECT * FROM inventories WHERE id = ?", inventId).Scan(&updatedImageInventory).Error
+	if err != nil {
+		return models.Inventory{}, err
+	}
+	return updatedImageInventory, nil
 }
