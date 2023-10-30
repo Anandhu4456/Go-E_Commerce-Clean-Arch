5589e5a81a47593e49f117740f4e6cc21222e4a2
Anandhu4456 <anandhurajan56@gmail.com>
2023-10-30 18:19:27 +0530

    created add inventory function in inventory repo

--- pkg/repository/inventory.go
+++ pkg/repository/inventory.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"github.com/Anandhu4456/go-Ecommerce/pkg/repository/interfaces"
+	"github.com/Anandhu4456/go-Ecommerce/pkg/utils/models"
 	"gorm.io/gorm"
 )
 
@@ -15,4 +16,17 @@ func NewInventoryRepository(DB *gorm.DB) interfaces.InventoryRespository {
 	return &inventoryRespository{
 		DB: DB,
 	}
+}
+
+func (ir *inventoryRespository) AddInventory(inventory models.Inventory, url string) (models.InventoryResponse, error) {
+	var inventoryResp models.InventoryResponse
+
+	query := `INSERT INTO inventories (categori_id,product_name,description,stock,price,image)
+	VALUES(?,?,?,?,?,?)RETURNING id`
+
+	err := ir.DB.Raw(query, inventory.CategoryID, inventory.ProductName, inventory.Description, inventory.Stock, inventory.Price, inventory.Image, url).Scan(&inventoryResp.ProductID).Error
+	if err != nil {
+		return models.InventoryResponse{}, err
+	}
+	return models.InventoryResponse{}, nil
 }
