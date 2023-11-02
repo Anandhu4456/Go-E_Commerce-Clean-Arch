8b0ef8a0d767c2196ea20edda1a38e318e2fad09
Anandhu4456 <anandhurajan56@gmail.com>
2023-11-02 19:23:36 +0530

    created get orders function in order repo

--- pkg/repository/order.go
+++ pkg/repository/order.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	"github.com/Anandhu4456/go-Ecommerce/pkg/domain"
 	"github.com/Anandhu4456/go-Ecommerce/pkg/repository/interfaces"
 	"gorm.io/gorm"
 )
@@ -10,9 +11,26 @@ type orderRepository struct {
 }
 
 // constructor function
-func NewOrderRepository(DB *gorm.DB)interfaces.OrderRepository{
+func NewOrderRepository(DB *gorm.DB) interfaces.OrderRepository {
 	return &orderRepository{
-		DB:DB,
+		DB: DB,
 	}
 }
 
+func (orr *orderRepository) GetOrders(id, page, limit int) ([]domain.Order, error) {
+	if page == 0 {
+		page = 1
+	}
+	if limit == 0 {
+		limit = 10
+	}
+	offset := (page - 1) * limit
+
+	var getOrders []domain.Order
+
+	err := orr.DB.Raw("SELECT * FROM orders WHERE id= ? limit ? offset ?", id, limit, offset).Scan(&getOrders).Error
+	if err != nil {
+		return []domain.Order{}, err
+	}
+	return getOrders, nil
+}
