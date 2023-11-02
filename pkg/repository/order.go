d5dd8dfedc1d0aa878d9ae2a2facc2ed48d1b8e1
Anandhu4456 <anandhurajan56@gmail.com>
2023-11-02 19:12:23 +0530

    created a constructor function for order repo

--- pkg/repository/order.go
+++ pkg/repository/order.go
@@ -1,1 +1,18 @@
 package repository
+
+import (
+	"github.com/Anandhu4456/go-Ecommerce/pkg/repository/interfaces"
+	"gorm.io/gorm"
+)
+
+type orderRepository struct {
+	DB *gorm.DB
+}
+
+// constructor function
+func NewOrderRepository(DB *gorm.DB)interfaces.OrderRepository{
+	return &orderRepository{
+		DB:DB,
+	}
+}
+
