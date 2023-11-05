e66bf0a118acd0aa9619041353f608becc76014d
Anandhu4456 <anandhurajan56@gmail.com>
2023-11-05 13:00:15 +0530

    created a constructor function for payment repository in payment repo

--- pkg/repository/payment.go
+++ pkg/repository/payment.go
@@ -1,1 +1,18 @@
 package repository
+
+import (
+	"github.com/Anandhu4456/go-Ecommerce/pkg/repository/interfaces"
+	"gorm.io/gorm"
+)
+
+type paymentRepository struct {
+	DB *gorm.DB
+}
+
+// constructor function
+
+func NewPaymentRepository(DB *gorm.DB) interfaces.PaymentRepository {
+	return &paymentRepository{
+		DB: DB,
+	}
+}
