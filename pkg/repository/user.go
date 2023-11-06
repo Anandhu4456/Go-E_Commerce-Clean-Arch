aa8ffa5e0103477d068b0d15c3ae08d383c2311d
Anandhu4456 <anandhurajan56@gmail.com>
2023-11-06 18:05:44 +0530

    created user block status funciton in user repo

--- pkg/repository/user.go
+++ pkg/repository/user.go
@@ -5,26 +5,34 @@ import (
 	"gorm.io/gorm"
 )
 
-
-type userRepository struct{
+type userRepository struct {
 	DB *gorm.DB
 }
 
 // constructor funciton
 
-func NewUserRepository(DB *gorm.DB)interfaces.UserRepository{
+func NewUserRepository(DB *gorm.DB) interfaces.UserRepository {
 	return &userRepository{
-		DB:DB,
+		DB: DB,
 	}
 }
 
-func (ur *userRepository)CheckUserAvailability(email string) bool{
+func (ur *userRepository) CheckUserAvailability(email string) bool {
 	var userCount int
 
-	err:=ur.DB.Raw("SELECT COUNT(*) FROM users WHERE email=?",email).Scan(&userCount).Error
-	if err!=nil{
+	err := ur.DB.Raw("SELECT COUNT(*) FROM users WHERE email=?", email).Scan(&userCount).Error
+	if err != nil {
 		return false
 	}
 	// if count greater than 0, user already exist
-	return userCount >0
+	return userCount > 0
+}
+
+func (ur *userRepository) UserBlockStatus(email string) (bool, error) {
+	var permission bool
+	err := ur.DB.Raw("SELECT permission FROM users WHERE email=?", email).Scan(&permission).Error
+	if err != nil {
+		return false, err
+	}
+	return permission, nil
 }
