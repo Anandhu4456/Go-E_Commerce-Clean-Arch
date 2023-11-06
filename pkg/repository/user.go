1cfc50b8d56d1d5b7abd3f357d177e1fe3b4cd3e
Anandhu4456 <anandhurajan56@gmail.com>
2023-11-06 13:54:59 +0530

    created a constructor function in user repo

--- pkg/repository/user.go
+++ pkg/repository/user.go
@@ -1,1 +1,19 @@
 package repository
+
+import (
+	"github.com/Anandhu4456/go-Ecommerce/pkg/repository/interfaces"
+	"gorm.io/gorm"
+)
+
+
+type userRepository struct{
+	DB *gorm.DB
+}
+
+// constructor funciton
+
+func NewUserRepository(DB *gorm.DB)interfaces.UserRepository{
+	return &userRepository{
+		DB:DB,
+	}
+}
