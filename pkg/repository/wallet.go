5ca8b5f424f8c72bd8694f09410b710998f828c1
Anandhu4456 <anandhurajan56@gmail.com>
2023-11-07 21:31:54 +0530

    created a constructor function for wallet repository in wallet repo

--- pkg/repository/wallet.go
+++ pkg/repository/wallet.go
@@ -1,1 +1,16 @@
 package repository
+
+import (
+	"github.com/Anandhu4456/go-Ecommerce/pkg/repository/interfaces"
+	"gorm.io/gorm"
+)
+
+type walletRepository struct{
+	DB *gorm.DB
+}
+// constructor function
+func NewWalletRepository(DB *gorm.DB)interfaces.WalletRepository{
+	return &walletRepository{
+		DB:DB,
+	}
+}
