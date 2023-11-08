f143ed27e825aecbe5414afe6a2cabafbbc0feba
Anandhu4456 <anandhurajan56@gmail.com>
2023-11-08 20:34:37 +0530

    created get wishlistid function in wishlist repo

--- pkg/repository/wishlist.go
+++ pkg/repository/wishlist.go
@@ -1,18 +1,28 @@
 package repository
 
 import (
+	"errors"
+
 	"github.com/Anandhu4456/go-Ecommerce/pkg/repository/interfaces"
 	"gorm.io/gorm"
 )
 
-type wishlistReposotory struct{
+type wishlistReposotory struct {
 	DB *gorm.DB
 }
 
-// constructor function 
+// constructor function
 
-func NewWishlistRepository(DB *gorm.DB)interfaces.WishlistRepository{
+func NewWishlistRepository(DB *gorm.DB) interfaces.WishlistRepository {
 	return &wishlistReposotory{
-		DB:DB,
+		DB: DB,
 	}
+}
+
+func (wlr *wishlistReposotory) GetWishlistId(user_id int) (int, error) {
+	var wishlistId int
+	if err := wlr.DB.Raw("SELECT id FROM wishlists WHERE user_id=?", user_id).Scan(&wishlistId).Error; err != nil {
+		return 0, errors.New("wishlist id not found")
+	}
+	return wishlistId, nil
 }
