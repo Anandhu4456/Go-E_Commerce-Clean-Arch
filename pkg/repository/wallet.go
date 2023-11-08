18caabea3e60499bf0530cffdba15efe125c88af
Anandhu4456 <anandhurajan56@gmail.com>
2023-11-08 13:36:17 +0530

    created credit to user wallet function in wallet repo

--- pkg/repository/wallet.go
+++ pkg/repository/wallet.go
@@ -1,16 +1,26 @@
 package repository
 
 import (
+	"errors"
+
 	"github.com/Anandhu4456/go-Ecommerce/pkg/repository/interfaces"
 	"gorm.io/gorm"
 )
 
-type walletRepository struct{
+type walletRepository struct {
 	DB *gorm.DB
 }
+
 // constructor function
-func NewWalletRepository(DB *gorm.DB)interfaces.WalletRepository{
+func NewWalletRepository(DB *gorm.DB) interfaces.WalletRepository {
 	return &walletRepository{
-		DB:DB,
+		DB: DB,
 	}
+}
+
+func (wr *walletRepository) CreditToUserWallet(amount float64, walletId int) error {
+	if err := wr.DB.Exec("UPDATE wallets SET amount=amount+$1 WHERE wallet_id=$2", amount, walletId).Error; err != nil {
+		return errors.New("amount adding to wallet failed")
+	}
+	return nil
 }
