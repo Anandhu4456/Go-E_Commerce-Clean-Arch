36c4252d2e5f754a55d5d9f2a2513f25a65ef623
Anandhu4456 <anandhurajan56@gmail.com>
2023-11-08 20:30:24 +0530

    created a constructor function for wishlist repository in wishlist repo

--- pkg/repository/wishlist.go
+++ pkg/repository/wishlist.go
@@ -1,1 +1,18 @@
 package repository
+
+import (
+	"github.com/Anandhu4456/go-Ecommerce/pkg/repository/interfaces"
+	"gorm.io/gorm"
+)
+
+type wishlistReposotory struct{
+	DB *gorm.DB
+}
+
+// constructor function 
+
+func NewWishlistRepository(DB *gorm.DB)interfaces.WishlistRepository{
+	return &wishlistReposotory{
+		DB:DB,
+	}
+}
