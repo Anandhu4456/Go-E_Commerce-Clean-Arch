010b7239a0640bf82a7c8a657194e3a6cdf9c27e
Anandhu4456 <anandhurajan56@gmail.com>
2023-11-09 18:48:22 +0530

    created cart usecase interface in usecase layer

--- pkg/usecase/interfaces/cart.go
+++ pkg/usecase/interfaces/cart.go
@@ -1,1 +1,8 @@
 package interfaces
+
+import "github.com/Anandhu4456/go-Ecommerce/pkg/utils/models"
+
+type CartUsecase interface {
+	AddToCart(user_id, inventory_id int) error
+	CheckOut(id int) (models.CheckOut, error)
+}
