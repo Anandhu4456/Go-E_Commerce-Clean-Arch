784e75c7b645079214d1ad67212a4037b98bfc7f
Anandhu4456 <anandhurajan56@gmail.com>
2023-11-09 19:20:33 +0530

    created user usecase interface in usecase layer

--- pkg/usecase/interfaces/user.go
+++ pkg/usecase/interfaces/user.go
@@ -1,1 +1,26 @@
 package interfaces
+
+import (
+	"github.com/Anandhu4456/go-Ecommerce/pkg/domain"
+	"github.com/Anandhu4456/go-Ecommerce/pkg/utils/models"
+)
+
+type UserUsecase interface {
+	Login(user models.UserLogin) (models.UserToken, error)
+	SignUp(user models.UserDetails) (models.UserToken, error)
+	AddAddress(id int, address models.AddAddress) error
+	GetAddresses(id int) ([]domain.Address, error)
+	GetUserDetails(id int) (models.UserResponse, error)
+
+	ChangePassword(id int, old string, password string, repassword string) error
+	EditUser(id int, userData models.EditUser) error
+
+	GetCartID(userID int) (int, error)
+	GetCart(id, page, limit int) ([]models.GetCart, error)
+	RemoveFromCart(id int, inventoryID int) error
+	ClearCart(cartID int) error
+	UpdateQuantityAdd(id, inv_id int) error
+	UpdateQuantityLess(id, inv_id int) error
+
+	GetWallet(id, page, limit int) (models.Wallet, error)
+}
