5b6b038ec4d7321803428420b96daf1b867ac991
Anandhu4456 <anandhurajan56@gmail.com>
2023-11-09 19:21:53 +0530

    created wishlist usecase interface in usecase layer

--- pkg/usecase/interfaces/wishlist.go
+++ pkg/usecase/interfaces/wishlist.go
@@ -1,1 +1,10 @@
 package interfaces
+
+import "github.com/Anandhu4456/go-Ecommerce/pkg/utils/models"
+
+type WishlistUsecase interface {
+	AddToWishlist(user_id, inventory_id int) error
+	GetWishlistID(userID int) (int, error)
+	GetWishlist(id int) ([]models.GetWishlist, error)
+	RemoveFromWishlist(id int, inventoryID int) error
+}
