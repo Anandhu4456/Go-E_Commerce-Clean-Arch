b989abc7da328a5cd5546bf6f24bdeeb8efe97ee
Anandhu4456 <anandhurajan56@gmail.com>
2023-11-09 18:43:34 +0530

    created admin usecase interface in usecase layer

--- pkg/usecase/interfaces/admin.go
+++ pkg/usecase/interfaces/admin.go
@@ -1,1 +1,10 @@
 package interfaces
+
+import "github.com/Anandhu4456/go-Ecommerce/pkg/utils/models"
+
+type AdminUsecase interface {
+	LoginHandler(adminDetails models.AdminLogin) (models.AdminToken, error)
+	BlockUser(id string) error
+	UnblockUser(id string) error
+	GetUsers(page, limit int) ([]models.UserDetailsAtAdmin, error)
+}
