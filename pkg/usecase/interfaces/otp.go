f558b567ffee21875f3106114524a26d3a249766
Anandhu4456 <anandhurajan56@gmail.com>
2023-11-09 19:17:17 +0530

    created otp usecase interface in usecase layer

--- pkg/usecase/interfaces/otp.go
+++ pkg/usecase/interfaces/otp.go
@@ -1,1 +1,8 @@
 package interfaces
+
+import "github.com/Anandhu4456/go-Ecommerce/pkg/utils/models"
+
+type OtpUsecase interface {
+	VerifyOTP(code models.VerifyData) (models.UserToken, error)
+	SendOTP(phone string) error
+}
