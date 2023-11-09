7f19ef8257745cb1bf8bc3277db7c5eb8fd98ab9
Anandhu4456 <anandhurajan56@gmail.com>
2023-11-09 19:10:53 +0530

    created offer usecase interface in usecase layer

--- pkg/usecase/interfaces/offer.go
+++ pkg/usecase/interfaces/offer.go
@@ -1,1 +1,12 @@
 package interfaces
+
+import (
+	"github.com/Anandhu4456/go-Ecommerce/pkg/domain"
+	"github.com/Anandhu4456/go-Ecommerce/pkg/utils/models"
+)
+
+type OfferUsecase interface {
+	AddNewOffer(model models.CreateOffer) error
+	MakeOfferExpire(catID int) error
+	GetOffers(page, limit int) ([]domain.Offer, error)
+}
