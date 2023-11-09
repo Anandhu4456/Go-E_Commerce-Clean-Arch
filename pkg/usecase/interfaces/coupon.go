0db40613f3b4b97694d167cfff147f0609d2fbff
Anandhu4456 <anandhurajan56@gmail.com>
2023-11-09 18:57:44 +0530

    created coupon usecase interface in usecase layer

--- pkg/usecase/interfaces/coupon.go
+++ pkg/usecase/interfaces/coupon.go
@@ -1,1 +1,9 @@
 package interfaces
+
+import "github.com/Anandhu4456/go-Ecommerce/pkg/domain"
+
+type CouponUsecase interface {
+	Addcoupon(coupon domain.Coupon) error
+	MakeCouponInvalid(id int) error
+	GetCoupons(page, limit int) ([]domain.Coupon, error)
+}
