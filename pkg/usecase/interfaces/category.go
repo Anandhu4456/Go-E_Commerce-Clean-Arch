007a692e77c6ecc8f756f46ec51d2304e70671d6
Anandhu4456 <anandhurajan56@gmail.com>
2023-11-09 18:53:38 +0530

    created category usecase interface in usecase layer

--- pkg/usecase/interfaces/category.go
+++ pkg/usecase/interfaces/category.go
@@ -1,1 +1,12 @@
 package interfaces
+
+import (
+	"github.com/Anandhu4456/go-Ecommerce/pkg/domain"
+)
+
+type CategoryUsecase interface {
+	AddCategory(category string) (domain.Category, error)
+	UpdateCategory(currrent, new string) (domain.Category, error)
+	DeleteCategory(categoryId string) error
+	GetCategories(page, limit int) ([]domain.Category, error)
+}
