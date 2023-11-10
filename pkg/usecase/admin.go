3373d05c105f682cc0ac041604454e4d71f8c3f4
Anandhu4456 <anandhurajan56@gmail.com>
2023-11-10 17:55:48 +0530

    created a constructor funciton in admin usecase layer

--- pkg/usecase/admin.go
+++ pkg/usecase/admin.go
@@ -1,1 +1,16 @@
 package usecase
+
+import ("github.com/Anandhu4456/go-Ecommerce/pkg/repository/interfaces"
+	services "github.com/Anandhu4456/go-Ecommerce/pkg/usecase/interfaces"
+)
+
+type adminUsecase struct {
+	adminRepository interfaces.AdminRepository
+}
+
+// constructor function
+func NewAdminUsecase(adRepo interfaces.AdminRepository)services.AdminUsecase{
+	return &adminUsecase{
+		adminRepository: adRepo,
+	}
+}
