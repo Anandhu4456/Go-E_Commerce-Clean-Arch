14ba6ea0ef6f31b3850e515a48a921cfdb82af3d
Anandhu4456 <anandhurajan56@gmail.com>
2023-11-10 18:57:02 +0530

    created login handler function in admin usecase

--- pkg/usecase/admin.go
+++ pkg/usecase/admin.go
@@ -1,7 +1,13 @@
 package usecase
 
-import ("github.com/Anandhu4456/go-Ecommerce/pkg/repository/interfaces"
+import (
+	"errors"
+
+	"github.com/Anandhu4456/go-Ecommerce/pkg/helper"
+	"github.com/Anandhu4456/go-Ecommerce/pkg/repository/interfaces"
 	services "github.com/Anandhu4456/go-Ecommerce/pkg/usecase/interfaces"
+	"github.com/Anandhu4456/go-Ecommerce/pkg/utils/models"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type adminUsecase struct {
@@ -9,8 +15,32 @@ type adminUsecase struct {
 }
 
 // constructor function
-func NewAdminUsecase(adRepo interfaces.AdminRepository)services.AdminUsecase{
+func NewAdminUsecase(adRepo interfaces.AdminRepository) services.AdminUsecase {
 	return &adminUsecase{
 		adminRepository: adRepo,
 	}
+}
+
+func (au *adminUsecase) LoginHandler(adminDetails models.AdminLogin) (models.AdminToken, error) {
+	// Getting details of the admin based on the email provided
+
+	adminCompareDetails, err := au.adminRepository.LoginHandler(adminDetails)
+	if err != nil {
+		return models.AdminToken{}, errors.New("admin not found")
+	}
+	// Compare password from database that provided by admin
+
+	err = bcrypt.CompareHashAndPassword([]byte(adminCompareDetails.Password), []byte(adminDetails.Password))
+	if err != nil {
+		return models.AdminToken{}, errors.New("password doesn't match")
+	}
+
+	token, err := helper.GenerateAdminToken(adminCompareDetails)
+	if err != nil {
+		return models.AdminToken{}, err
+	}
+	return models.AdminToken{
+		Username: adminCompareDetails.Username,
+		Token:    token,
+	}, nil
 }
