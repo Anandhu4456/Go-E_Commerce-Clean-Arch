d512b23e6af7ff638624409d2f8b5b39836b8479
Anandhu4456 <anandhurajan56@gmail.com>
2023-11-11 18:56:06 +0530

    created add to cart function in cart usecase

--- pkg/usecase/cart.go
+++ pkg/usecase/cart.go
@@ -1,6 +1,8 @@
 package usecase
 
 import (
+	"errors"
+
 	interfaces "github.com/Anandhu4456/go-Ecommerce/pkg/repository/interfaces"
 	services "github.com/Anandhu4456/go-Ecommerce/pkg/usecase/interfaces"
 )
@@ -21,4 +23,43 @@ func NewCartUsecase(cartRepo interfaces.CartRepository, invRepo interfaces.Inven
 		userUsecase:    userUsecase,
 		paymentUsecase: paymentUsecase,
 	}
+}
+
+func (cu *cartUsecase) AddToCart(user_id, inventory_id int) error {
+
+	// check the product has quantity available
+	stock, err := cu.invRepo.CheckStock(inventory_id)
+	if err != nil {
+		return errors.New("no stock")
+	}
+	if stock <= 0 {
+		return errors.New("out of stock")
+	}
+	// Find user cart id
+	cartId, err := cu.cartRepo.GetCartId(user_id)
+	if err != nil {
+		return errors.New("cart id not found")
+	}
+	// If user has no cart,create a cart
+	if cartId == 0 {
+		cartId, err = cu.cartRepo.CreateNewCart(user_id)
+		if err != nil {
+			return errors.New("cart creation failed")
+		}
+	}
+	// Check if already added
+
+	if cu.cartRepo.CheckIfInvAdded(inventory_id, cartId) {
+		err := cu.cartRepo.AddQuantity(inventory_id, cartId)
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+	// add product in line item
+	err = cu.cartRepo.AddLineItems(inventory_id, cartId)
+	if err != nil {
+		return errors.New("product adding failed")
+	}
+	return nil
 }
