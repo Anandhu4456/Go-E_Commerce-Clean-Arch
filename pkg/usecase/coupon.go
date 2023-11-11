7ca70bd3caf2e0468f77b3a5dc2335273ee66ca2
Anandhu4456 <anandhurajan56@gmail.com>
2023-11-11 23:51:52 +0530

    created add coupon function in coupon usecase

--- pkg/usecase/coupon.go
+++ pkg/usecase/coupon.go
@@ -1,6 +1,9 @@
 package usecase
 
 import (
+	"errors"
+
+	"github.com/Anandhu4456/go-Ecommerce/pkg/domain"
 	interfaces "github.com/Anandhu4456/go-Ecommerce/pkg/repository/interfaces"
 	services "github.com/Anandhu4456/go-Ecommerce/pkg/usecase/interfaces"
 )
@@ -11,8 +14,15 @@ type couponUsecase struct {
 
 // constructor function
 
-func NewCouponUsecase(couponRepo interfaces.CouponRepository) services.CouponUsecase{
+func NewCouponUsecase(couponRepo interfaces.CouponRepository) services.CouponUsecase {
 	return &couponUsecase{
 		couponRepo: couponRepo,
 	}
+}
+
+func (coupU *couponUsecase) Addcoupon(coupon domain.Coupon) error {
+	if err := coupU.couponRepo.AddCoupon(coupon); err != nil {
+		return errors.New("coupon adding failed")
+	}
+	return nil
 }
