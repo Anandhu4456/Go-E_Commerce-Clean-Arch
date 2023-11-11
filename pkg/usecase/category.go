4e2efb8e82ae673446465198e6f0371ea111bb98
Anandhu4456 <anandhurajan56@gmail.com>
2023-11-11 19:33:46 +0530

    created updatecategory funciton in category usecase

--- pkg/usecase/category.go
+++ pkg/usecase/category.go
@@ -1,6 +1,8 @@
 package usecase
 
 import (
+	"errors"
+
 	"github.com/Anandhu4456/go-Ecommerce/pkg/domain"
 	"github.com/Anandhu4456/go-Ecommerce/pkg/repository/interfaces"
 	services "github.com/Anandhu4456/go-Ecommerce/pkg/usecase/interfaces"
@@ -24,4 +26,19 @@ func (catU *categoryUsecase) AddCategory(category string) (domain.Category, erro
 		return domain.Category{}, err
 	}
 	return productResponse, nil
+}
+
+func (catU *categoryUsecase) UpdateCategory(currrent, new string) (domain.Category, error) {
+	result, err := catU.repo.CheckCategory(currrent)
+	if err != nil {
+		return domain.Category{}, err
+	}
+	if !result {
+		return domain.Category{}, errors.New("no category as you mentioned")
+	}
+	newCat, err := catU.UpdateCategory(currrent, new)
+	if err != nil {
+		return domain.Category{}, err
+	}
+	return newCat, nil
 }
