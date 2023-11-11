712e9b4a84ad2b76df5d916e770c9f7620185878
Anandhu4456 <anandhurajan56@gmail.com>
2023-11-11 19:28:38 +0530

    created add category function in category usecase

--- pkg/usecase/category.go
+++ pkg/usecase/category.go
@@ -1,17 +1,27 @@
 package usecase
 
-import ("github.com/Anandhu4456/go-Ecommerce/pkg/repository/interfaces"
+import (
+	"github.com/Anandhu4456/go-Ecommerce/pkg/domain"
+	"github.com/Anandhu4456/go-Ecommerce/pkg/repository/interfaces"
 	services "github.com/Anandhu4456/go-Ecommerce/pkg/usecase/interfaces"
 )
 
-type categoryUsecase struct{
+type categoryUsecase struct {
 	repo interfaces.CategoryRepository
 }
 
 // constructor function
 
-func NewCategoryUsecase(repo interfaces.CategoryRepository)services.CategoryUsecase{
+func NewCategoryUsecase(repo interfaces.CategoryRepository) services.CategoryUsecase {
 	return categoryUsecase{
-		repo:repo,
+		repo: repo,
 	}
+}
+
+func (catU *categoryUsecase) AddCategory(category string) (domain.Category, error) {
+	productResponse, err := catU.repo.AddCategory(category)
+	if err != nil {
+		return domain.Category{}, err
+	}
+	return productResponse, nil
 }
