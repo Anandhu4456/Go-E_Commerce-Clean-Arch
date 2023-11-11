0a50b619477b35a1f7268a671b6c127dde7a857c
Anandhu4456 <anandhurajan56@gmail.com>
2023-11-11 23:48:40 +0530

    created a constructor function in coupon usecase

--- pkg/usecase/coupon.go
+++ pkg/usecase/coupon.go
@@ -1,1 +1,18 @@
 package usecase
+
+import (
+	interfaces "github.com/Anandhu4456/go-Ecommerce/pkg/repository/interfaces"
+	services "github.com/Anandhu4456/go-Ecommerce/pkg/usecase/interfaces"
+)
+
+type couponUsecase struct {
+	couponRepo interfaces.CouponRepository
+}
+
+// constructor function
+
+func NewCouponUsecase(couponRepo interfaces.CouponRepository) services.CouponUsecase{
+	return &couponUsecase{
+		couponRepo: couponRepo,
+	}
+}
