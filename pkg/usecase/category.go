7e1bb30b0db99e9ce3e452412522f8447acd0e96
Anandhu4456 <anandhurajan56@gmail.com>
2023-11-11 19:25:22 +0530

    created a constructor function in category usecase

--- pkg/usecase/category.go
+++ pkg/usecase/category.go
@@ -1,1 +1,17 @@
 package usecase
+
+import ("github.com/Anandhu4456/go-Ecommerce/pkg/repository/interfaces"
+	services "github.com/Anandhu4456/go-Ecommerce/pkg/usecase/interfaces"
+)
+
+type categoryUsecase struct{
+	repo interfaces.CategoryRepository
+}
+
+// constructor function
+
+func NewCategoryUsecase(repo interfaces.CategoryRepository)services.CategoryUsecase{
+	return categoryUsecase{
+		repo:repo,
+	}
+}
