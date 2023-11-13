565000ad695f423d2199a6eb0a8a36d0a9063522
Anandhu4456 <anandhurajan56@gmail.com>
2023-11-13 20:10:44 +0530

    created a constructor function in offer usecase

--- pkg/usecase/offer.go
+++ pkg/usecase/offer.go
@@ -1,1 +1,17 @@
 package usecase
+
+import (
+	interfaces"github.com/Anandhu4456/go-Ecommerce/pkg/repository/interfaces"
+	services"github.com/Anandhu4456/go-Ecommerce/pkg/usecase/interfaces"
+)
+
+type offerUsecase struct{
+	offerRepo interfaces.OfferRepository
+}
+// constructor function
+
+func NewOfferUsecase(offerRepo interfaces.OfferRepository)services.OfferUsecase{
+	return &offerUsecase{
+		offerRepo: offerRepo,
+	}
+}
