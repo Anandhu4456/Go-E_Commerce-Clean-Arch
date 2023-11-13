0dbf2555477a97b72e1a7c166795066272f890b5
Anandhu4456 <anandhurajan56@gmail.com>
2023-11-13 20:21:25 +0530

    created getoffers function in offer usecase

--- pkg/usecase/offer.go
+++ pkg/usecase/offer.go
@@ -3,6 +3,7 @@ package usecase
 import (
 	"errors"
 
+	"github.com/Anandhu4456/go-Ecommerce/pkg/domain"
 	interfaces "github.com/Anandhu4456/go-Ecommerce/pkg/repository/interfaces"
 	services "github.com/Anandhu4456/go-Ecommerce/pkg/usecase/interfaces"
 	"github.com/Anandhu4456/go-Ecommerce/pkg/utils/models"
@@ -32,4 +33,12 @@ func (offU *offerUsecase) MakeOfferExpire(catID int) error {
 		return err
 	}
 	return nil
+}
+
+func (offU *offerUsecase) GetOffers(page, limit int) ([]domain.Offer, error) {
+	offers, err := offU.offerRepo.GetOffers(page, limit)
+	if err != nil {
+		return []domain.Offer{}, err
+	}
+	return offers, nil
 }
