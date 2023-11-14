212315aac8791222b1b0a16a7952926debf2cea0
Anandhu4456 <anandhurajan56@gmail.com>
2023-11-14 18:04:17 +0530

    created getorders function in order usecase

--- pkg/usecase/order.go
+++ pkg/usecase/order.go
@@ -1,22 +1,31 @@
 package usecase
 
-import ("github.com/Anandhu4456/go-Ecommerce/pkg/repository/interfaces"
-	services"github.com/Anandhu4456/go-Ecommerce/pkg/usecase/interfaces"
-
+import (
+	"github.com/Anandhu4456/go-Ecommerce/pkg/domain"
+	"github.com/Anandhu4456/go-Ecommerce/pkg/repository/interfaces"
+	services "github.com/Anandhu4456/go-Ecommerce/pkg/usecase/interfaces"
 )
 
-type orderUsecase struct{
-	orderRepo interfaces.OrderRepository
+type orderUsecase struct {
+	orderRepo   interfaces.OrderRepository
 	userUsecase services.UserUsecase
-	walletRepo interfaces.WalletRepository
-	couponRepo interfaces.CouponRepository
+	walletRepo  interfaces.WalletRepository
+	couponRepo  interfaces.CouponRepository
 }
 
-func NewOrderUsecase(orderRepo interfaces.OrderRepository, userUsecase services.UserUsecase,walletRepo interfaces.WalletRepository,couponRepo interfaces.CouponRepository)*orderUsecase{
+func NewOrderUsecase(orderRepo interfaces.OrderRepository, userUsecase services.UserUsecase, walletRepo interfaces.WalletRepository, couponRepo interfaces.CouponRepository) *orderUsecase {
 	return &orderUsecase{
-		orderRepo: orderRepo,
+		orderRepo:   orderRepo,
 		userUsecase: userUsecase,
-		walletRepo: walletRepo,
-		couponRepo: couponRepo,
+		walletRepo:  walletRepo,
+		couponRepo:  couponRepo,
 	}
+}
+
+func (orU *orderUsecase) GetOrders(id, page, limit int) ([]domain.Order, error) {
+	orders, err := orU.orderRepo.GetOrders(id, page, limit)
+	if err != nil {
+		return []domain.Order{}, err
+	}
+	return orders, nil
 }
