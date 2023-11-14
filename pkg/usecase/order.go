79c7281e876f46969041060ee53eeafc8c4db740
Anandhu4456 <anandhurajan56@gmail.com>
2023-11-14 17:47:41 +0530

    created orderusecase struct in order usecase

--- pkg/usecase/order.go
+++ pkg/usecase/order.go
@@ -1,1 +1,13 @@
 package usecase
+
+import ("github.com/Anandhu4456/go-Ecommerce/pkg/repository/interfaces"
+	services"github.com/Anandhu4456/go-Ecommerce/pkg/usecase/interfaces"
+
+)
+
+type orderUsecase struct{
+	orderRepo interfaces.OrderRepository
+	userUsecase services.UserUsecase
+	walletRepo interfaces.WalletRepository
+	couponRepo interfaces.CouponRepository
+}
