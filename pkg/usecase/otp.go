62881a17fa4298a1a611a6ecdc0f7ecf847d9c87
Anandhu4456 <anandhurajan56@gmail.com>
2023-11-17 23:14:44 +0530

    created send otp function with help of twilo in otp usecase

--- pkg/usecase/otp.go
+++ pkg/usecase/otp.go
@@ -1,7 +1,10 @@
 package usecase
 
 import (
+	"errors"
+
 	"github.com/Anandhu4456/go-Ecommerce/pkg/config"
+	"github.com/Anandhu4456/go-Ecommerce/pkg/helper"
 	interfaces "github.com/Anandhu4456/go-Ecommerce/pkg/repository/interfaces"
 	services "github.com/Anandhu4456/go-Ecommerce/pkg/usecase/interfaces"
 )
@@ -17,4 +20,17 @@ func NewOtpUsecase(cfg config.Config, otpRepo interfaces.OtpRepository) services
 		cfg:     cfg,
 		otpRepo: otpRepo,
 	}
+}
+
+func (otU *otpUsecase) SendOTP(phone string) error {
+	ok := otU.otpRepo.FindUserByMobileNumber(phone)
+	if !ok {
+		return errors.New("phone number not found")
+	}
+	helper.TwilioSetup(otU.cfg.ACCOUNTSID, otU.cfg.AUTHTOKEN)
+	_, err := helper.TwilioSendOTP(phone, otU.cfg.SERVICEID)
+	if err != nil {
+		return errors.New("error occured while generating OTP")
+	}
+	return nil
 }
