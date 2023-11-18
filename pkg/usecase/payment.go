bca5a1321158a6206d494e073a901f9933a22f40
Anandhu4456 <anandhurajan56@gmail.com>
2023-11-18 12:38:30 +0530

    created a constructor function in payment usecase

--- pkg/usecase/payment.go
+++ pkg/usecase/payment.go
@@ -1,1 +1,18 @@
 package usecase
+
+import (
+	interfaces"github.com/Anandhu4456/go-Ecommerce/pkg/repository/interfaces"
+	services"github.com/Anandhu4456/go-Ecommerce/pkg/usecase/interfaces"
+)
+
+type paymentUsecase struct{
+	paymentRepo interfaces.PaymentRepository
+	userRepo interfaces.UserRepository
+}
+// Constructor function
+func NewPaymentUsecase(paymentRepo interfaces.PaymentRepository,userRepo interfaces.UserRepository)services.PaymentUsecase{
+	return &paymentUsecase{
+		paymentRepo: paymentRepo,
+		userRepo: userRepo,
+	}
+}
