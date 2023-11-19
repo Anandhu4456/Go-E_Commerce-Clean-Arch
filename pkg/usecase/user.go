ef8f244d0b36476f89e991a9b7b8534ebfc5f06e
Anandhu4456 <anandhurajan56@gmail.com>
2023-11-19 22:15:18 +0530

    created a constructor function in user usecase

--- pkg/usecase/user.go
+++ pkg/usecase/user.go
@@ -1,1 +1,20 @@
 package usecase
+
+import (
+	interfaces"github.com/Anandhu4456/go-Ecommerce/pkg/repository/interfaces"
+	services"github.com/Anandhu4456/go-Ecommerce/pkg/usecase/interfaces"
+)
+type userUsecase struct{
+	userRepo interfaces.UserRepository
+	offerRepo interfaces.OfferRepository
+	walletRepo interfaces.WalletRepository
+}
+
+// Constructor function
+func NewUserUsecase(userRepo interfaces.UserRepository,offerRepo interfaces.OfferRepository,walletRepo interfaces.WalletRepository)services.UserUsecase{
+	return &userUsecase{
+		userRepo: userRepo,
+		offerRepo: offerRepo,
+		walletRepo: walletRepo,
+	}
+}
