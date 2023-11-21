07e74c91d5a895e30713cc2898072333ecb1f9fd
Anandhu4456 <anandhurajan56@gmail.com>
2023-11-21 16:38:47 +0530

    created add to cart function in cart handler

--- pkg/api/handlers/cart.go
+++ pkg/api/handlers/cart.go
@@ -1,7 +1,13 @@
 package handlers
 
 import (
+	"net/http"
+	"strconv"
+
+	"github.com/Anandhu4456/go-Ecommerce/pkg/helper"
 	services "github.com/Anandhu4456/go-Ecommerce/pkg/usecase/interfaces"
+	"github.com/Anandhu4456/go-Ecommerce/pkg/utils/response"
+	"github.com/gin-gonic/gin"
 )
 
 type CartHandler struct {
@@ -13,4 +19,28 @@ func NewCartHandler(usecase services.CartUsecase) *CartHandler {
 	return &CartHandler{
 		cartUsecase: usecase,
 	}
+}
+
+func (ch *CartHandler) AddtoCart(c *gin.Context) {
+	userId, err := helper.GetUserId(c)
+	if err != nil {
+		errorRes := response.ClientResponse(http.StatusBadRequest, "Could not get userID", nil, err.Error())
+		c.JSON(http.StatusBadRequest, errorRes)
+		return
+	}
+	inventoryID, err := strconv.Atoi(c.Query("inventory"))
+	if err != nil {
+		errorRes := response.ClientResponse(http.StatusBadRequest, "check parameters properly", nil, err.Error())
+		c.JSON(http.StatusBadRequest, errorRes)
+		return
+	}
+
+	if err := ch.cartUsecase.AddToCart(userId, inventoryID); err != nil {
+		errorRes := response.ClientResponse(http.StatusBadRequest, "Could not add the Cart", nil, err.Error())
+		c.JSON(http.StatusBadRequest, errorRes)
+		return
+	}
+
+	successRes := response.ClientResponse(http.StatusOK, "Successfully added To cart", nil, nil)
+	c.JSON(http.StatusOK, successRes)
 }
