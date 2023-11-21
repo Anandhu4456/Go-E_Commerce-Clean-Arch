7300ab288cd43d8bb719673f9e8ba97ba5742609
Anandhu4456 <anandhurajan56@gmail.com>
2023-11-21 14:27:16 +0530

    created login handler function in admin handler

--- pkg/api/handlers/admin.go
+++ pkg/api/handlers/admin.go
@@ -1,7 +1,11 @@
 package handlers
 
 import (
+	"net/http"
+
 	services "github.com/Anandhu4456/go-Ecommerce/pkg/usecase/interfaces"
+	"github.com/Anandhu4456/go-Ecommerce/pkg/utils/models"
+	"github.com/Anandhu4456/go-Ecommerce/pkg/utils/response"
 	"github.com/gin-gonic/gin"
 )
 
@@ -16,6 +20,13 @@ func NewAdminHandler(adminUsecase services.AdminUsecase) *AdminHandler {
 	}
 }
 
-func (ah *AdminHandler) LoginHandler(c *gin.Context){
+func (ah *AdminHandler) LoginHandler(c *gin.Context) {
+	// login handler for the admin
+	var adminDetails models.AdminLogin
 
+	if err := c.BindJSON(&adminDetails); err != nil {
+		errRes := response.ClientResponse(http.StatusBadRequest, "details not in correct format", nil, err.Error())
+		c.JSON(http.StatusBadRequest, errRes)
+		return
+	}
 }
