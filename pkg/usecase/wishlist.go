f43c2ea3aaf61898f42a93c2fde6ab87a6836add
Anandhu4456 <anandhurajan56@gmail.com>
2023-11-21 13:00:12 +0530

    created a constructor function in wishlist usecase

--- pkg/usecase/wishlist.go
+++ pkg/usecase/wishlist.go
@@ -1,1 +1,16 @@
 package usecase
+
+import (
+	interfaces"github.com/Anandhu4456/go-Ecommerce/pkg/repository/interfaces"
+	services"github.com/Anandhu4456/go-Ecommerce/pkg/usecase/interfaces"
+)
+
+type wishlistUsecase struct{
+	wishRepo interfaces.WishlistRepository
+}
+//Constructor function 
+func NewWishlistUsecase(wishRepo interfaces.WishlistRepository)services.WishlistUsecase{
+	return &wishlistUsecase{
+		wishRepo: wishRepo,
+	}
+}
