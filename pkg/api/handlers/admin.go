4fb1ec74a7bdc0ca7675151545347a9280b3d61d
Anandhu4456 <anandhurajan56@gmail.com>
2023-11-21 14:16:36 +0530

    added gin framework

--- pkg/api/handlers/admin.go
+++ pkg/api/handlers/admin.go
@@ -1,15 +1,22 @@
 package handlers
 
 import (
-	services"github.com/Anandhu4456/go-Ecommerce/pkg/usecase/interfaces"
+	services "github.com/Anandhu4456/go-Ecommerce/pkg/usecase/interfaces"
+	"github.com/Anandhu4456/go-Ecommerce/pkg/utils/models"
+	"github.com/gin-gonic/gin"
 )
 
-type AdminHandler struct{
-	adminUsecase services.AdminUsecase 
+type AdminHandler struct {
+	adminUsecase services.AdminUsecase
 }
+
 // constructor function
-func NewAdminHandler(adminUsecase services.AdminUsecase)*AdminHandler{
+func NewAdminHandler(adminUsecase services.AdminUsecase) *AdminHandler {
 	return &AdminHandler{
 		adminUsecase: adminUsecase,
 	}
+}
+
+func (ah *AdminHandler) LoginHandler(c *gin.Context){
+
 }
