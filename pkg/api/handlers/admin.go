2ac9c81309b7e0da8be339491eeb528dd44c7b06
Anandhu4456 <anandhurajan56@gmail.com>
2023-11-21 14:12:05 +0530

    created a constructor function in admin handler

--- pkg/api/handlers/admin.go
+++ pkg/api/handlers/admin.go
@@ -1,1 +1,15 @@
 package handlers
+
+import (
+	services"github.com/Anandhu4456/go-Ecommerce/pkg/usecase/interfaces"
+)
+
+type AdminHandler struct{
+	adminUsecase services.AdminUsecase 
+}
+// constructor function
+func NewAdminHandler(adminUsecase services.AdminUsecase)*AdminHandler{
+	return &AdminHandler{
+		adminUsecase: adminUsecase,
+	}
+}
