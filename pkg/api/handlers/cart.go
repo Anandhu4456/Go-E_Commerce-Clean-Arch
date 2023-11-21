e4f9e0e4e8ce990e02ca5edc92e36b218c2eec19
Anandhu4456 <anandhurajan56@gmail.com>
2023-11-21 16:26:31 +0530

    created a constructor function in cart handler

--- pkg/api/handlers/cart.go
+++ pkg/api/handlers/cart.go
@@ -1,1 +1,16 @@
 package handlers
+
+import (
+	services "github.com/Anandhu4456/go-Ecommerce/pkg/usecase/interfaces"
+)
+
+type CartHandler struct {
+	cartUsecase services.CartUsecase
+}
+
+// Constructor function
+func NewCartHandler(usecase services.CartUsecase) *CartHandler {
+	return &CartHandler{
+		cartUsecase: usecase,
+	}
+}
