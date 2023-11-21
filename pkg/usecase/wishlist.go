bcc254e454ccf255a8bc874c735dfdd632390819
Anandhu4456 <anandhurajan56@gmail.com>
2023-11-21 13:04:05 +0530

    created getwishlist id function in wishlist usecase

--- pkg/usecase/wishlist.go
+++ pkg/usecase/wishlist.go
@@ -1,16 +1,25 @@
 package usecase
 
 import (
-	interfaces"github.com/Anandhu4456/go-Ecommerce/pkg/repository/interfaces"
-	services"github.com/Anandhu4456/go-Ecommerce/pkg/usecase/interfaces"
+	interfaces "github.com/Anandhu4456/go-Ecommerce/pkg/repository/interfaces"
+	services "github.com/Anandhu4456/go-Ecommerce/pkg/usecase/interfaces"
 )
 
-type wishlistUsecase struct{
+type wishlistUsecase struct {
 	wishRepo interfaces.WishlistRepository
 }
-//Constructor function 
-func NewWishlistUsecase(wishRepo interfaces.WishlistRepository)services.WishlistUsecase{
+
+// Constructor function
+func NewWishlistUsecase(wishRepo interfaces.WishlistRepository) services.WishlistUsecase {
 	return &wishlistUsecase{
 		wishRepo: wishRepo,
 	}
+}
+
+func (wlU *wishlistUsecase) GetWishlistID(userID int) (int, error) {
+	wishlistId, err := wlU.wishRepo.GetWishlistId(userID)
+	if err != nil {
+		return 0, err
+	}
+	return wishlistId, nil
 }
