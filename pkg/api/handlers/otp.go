15647482f2f3b8e0e4ff96078c297087c03e46d6
Anandhu4456 <anandhurajan56@gmail.com>
2023-11-22 16:47:32 +0530

    created a constructor function in otp handler

--- pkg/api/handlers/otp.go
+++ pkg/api/handlers/otp.go
@@ -1,1 +1,16 @@
 package handlers
+
+import (
+	services "github.com/Anandhu4456/go-Ecommerce/pkg/usecase/interfaces"
+)
+
+type OtpHandler struct {
+	otpUsecase services.OtpUsecase
+}
+
+// Constrctor function
+func NewOtpHandler(otpUsecase services.OtpUsecase) *OtpHandler {
+	return &OtpHandler{
+		otpUsecase: otpUsecase,
+	}
+}
