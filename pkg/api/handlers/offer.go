76526eb59b3f23720fb820e523d248a5169e9abe
Anandhu4456 <anandhurajan56@gmail.com>
2023-11-22 15:17:21 +0530

    created add offer function in offer handler

--- pkg/api/handlers/offer.go
+++ pkg/api/handlers/offer.go
@@ -1,7 +1,12 @@
 package handlers
 
 import (
+	"net/http"
+
 	services "github.com/Anandhu4456/go-Ecommerce/pkg/usecase/interfaces"
+	"github.com/Anandhu4456/go-Ecommerce/pkg/utils/models"
+	"github.com/Anandhu4456/go-Ecommerce/pkg/utils/response"
+	"github.com/gin-gonic/gin"
 )
 
 type OfferHandler struct {
@@ -14,4 +19,21 @@ func NewOfferHandler(offerUsecase services.OfferUsecase) *OfferHandler {
 	return &OfferHandler{
 		offerUsecase: offerUsecase,
 	}
+}
+
+func (offH *OfferHandler) AddOffer(c *gin.Context) {
+	var offer models.CreateOffer
+
+	if err := c.BindJSON(&offer); err != nil {
+		errRes := response.ClientResponse(http.StatusBadRequest, "fields provided are in wrong format", nil, err.Error())
+		c.JSON(http.StatusBadRequest, errRes)
+		return
+	}
+	if err := offH.offerUsecase.AddNewOffer(offer); err != nil {
+		errRes := response.ClientResponse(http.StatusBadRequest, "offer adding failed", nil, err.Error())
+		c.JSON(http.StatusBadRequest, errRes)
+		return
+	}
+	successRes := response.ClientResponse(http.StatusOK, "offer added successfully", nil, nil)
+	c.JSON(http.StatusOK, successRes)
 }
