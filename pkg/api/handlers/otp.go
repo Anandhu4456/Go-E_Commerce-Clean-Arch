2f368eb11ec06496eba1b9ae48e827fea57bc953
Anandhu4456 <anandhurajan56@gmail.com>
2023-11-22 17:00:38 +0530

    created send otp function in otp handler

--- pkg/api/handlers/otp.go
+++ pkg/api/handlers/otp.go
@@ -1,7 +1,12 @@
 package handlers
 
 import (
+	"net/http"
+
 	services "github.com/Anandhu4456/go-Ecommerce/pkg/usecase/interfaces"
+	"github.com/Anandhu4456/go-Ecommerce/pkg/utils/models"
+	"github.com/Anandhu4456/go-Ecommerce/pkg/utils/response"
+	"github.com/gin-gonic/gin"
 )
 
 type OtpHandler struct {
@@ -13,4 +18,21 @@ func NewOtpHandler(otpUsecase services.OtpUsecase) *OtpHandler {
 	return &OtpHandler{
 		otpUsecase: otpUsecase,
 	}
+}
+
+func (otH *OtpHandler) SendOTP(c *gin.Context) {
+	var phone models.OTPData
+
+	if err := c.BindJSON(&phone); err != nil {
+		errRes := response.ClientResponse(http.StatusBadRequest, "fields provided are in wrong format", nil, err.Error())
+		c.JSON(http.StatusBadRequest, errRes)
+		return
+	}
+	if err := otH.otpUsecase.SendOTP(phone.PhoneNumber); err != nil {
+		errRes := response.ClientResponse(http.StatusBadRequest, "Could not sent OTP", nil, err.Error())
+		c.JSON(http.StatusBadRequest, errRes)
+		return
+	}
+	successRes := response.ClientResponse(http.StatusOK, "OTP send successfully", nil, nil)
+	c.JSON(http.StatusOK, successRes)
 }
