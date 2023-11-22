e53b78c0a4c61cb5764d89c2398f68747b16927b
Anandhu4456 <anandhurajan56@gmail.com>
2023-11-22 15:01:18 +0530

    created a constructor function in offer handler

--- pkg/api/handlers/offer.go
+++ pkg/api/handlers/offer.go
@@ -1,1 +1,17 @@
 package handlers
+
+import (
+	services "github.com/Anandhu4456/go-Ecommerce/pkg/usecase/interfaces"
+)
+
+type OfferHandler struct {
+	offerUsecase services.OfferUsecase
+}
+
+// Constructor function
+
+func NewOfferHandler(offerUsecase services.OfferUsecase) *OfferHandler {
+	return &OfferHandler{
+		offerUsecase: offerUsecase,
+	}
+}
