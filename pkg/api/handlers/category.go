af8a161ee043a9d8879f65f295fac926ce368aaa
Anandhu4456 <anandhurajan56@gmail.com>
2023-11-22 11:55:26 +0530

    created a constructor function in category handler

--- pkg/api/handlers/category.go
+++ pkg/api/handlers/category.go
@@ -1,1 +1,16 @@
 package handlers
+
+import (
+	services"github.com/Anandhu4456/go-Ecommerce/pkg/usecase/interfaces"
+)
+
+type CategoryHandler struct{
+	CategoryUsecase services.CategoryUsecase
+}
+// Constructor function
+
+func NewCategoryHandler(categoryUsecase services.CategoryUsecase)*CategoryHandler{
+	return &CategoryHandler{
+		CategoryUsecase: categoryUsecase,
+	}
+}
