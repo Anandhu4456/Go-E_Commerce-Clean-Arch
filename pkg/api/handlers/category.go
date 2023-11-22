4766ef2de0af65f7f20c6fbb338e1cf5c0df23fb
Anandhu4456 <anandhurajan56@gmail.com>
2023-11-22 12:18:09 +0530

    created update category function in category handler

--- pkg/api/handlers/category.go
+++ pkg/api/handlers/category.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	services "github.com/Anandhu4456/go-Ecommerce/pkg/usecase/interfaces"
+	"github.com/Anandhu4456/go-Ecommerce/pkg/utils/models"
 	"github.com/Anandhu4456/go-Ecommerce/pkg/utils/response"
 	"github.com/gin-gonic/gin"
 )
@@ -26,7 +27,28 @@ func (catH *CategoryHandler) AddCategory(c *gin.Context) {
 	if err != nil {
 		errRes := response.ClientResponse(http.StatusBadRequest, "Couldn't add category", nil, err.Error())
 		c.JSON(http.StatusBadRequest, errRes)
+		return
 	}
 	successRes := response.ClientResponse(http.StatusOK, "Category added successfully", categoryRes, nil)
+	c.JSON(http.StatusOK, successRes)
+	return
+}
+
+func (catH *CategoryHandler) UpdateCategory(c *gin.Context) {
+	var updateCategory models.SetNewName
+
+	if err := c.BindJSON(&updateCategory); err != nil {
+		errRes := response.ClientResponse(http.StatusBadRequest, "field provided are in wrong format", nil, err.Error())
+		c.JSON(http.StatusBadRequest, errRes)
+		return
+	}
+
+	updateRes, err := catH.CategoryUsecase.UpdateCategory(updateCategory.Current, updateCategory.New)
+	if err != nil {
+		errRes := response.ClientResponse(http.StatusBadRequest, "update category failed", nil, err.Error())
+		c.JSON(http.StatusBadRequest, errRes)
+		return
+	}
+	successRes := response.ClientResponse(http.StatusOK, "category updated successfully", updateRes, nil)
 	c.JSON(http.StatusOK, successRes)
 }
