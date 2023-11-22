f67acc92d5740a1bf9aa215ca2d4328b0d8177f0
Anandhu4456 <anandhurajan56@gmail.com>
2023-11-22 12:02:18 +0530

    created add category function in category handler

--- pkg/api/handlers/category.go
+++ pkg/api/handlers/category.go
@@ -1,16 +1,32 @@
 package handlers
 
 import (
-	services"github.com/Anandhu4456/go-Ecommerce/pkg/usecase/interfaces"
+	"net/http"
+
+	services "github.com/Anandhu4456/go-Ecommerce/pkg/usecase/interfaces"
+	"github.com/Anandhu4456/go-Ecommerce/pkg/utils/response"
+	"github.com/gin-gonic/gin"
 )
 
-type CategoryHandler struct{
+type CategoryHandler struct {
 	CategoryUsecase services.CategoryUsecase
 }
+
 // Constructor function
 
-func NewCategoryHandler(categoryUsecase services.CategoryUsecase)*CategoryHandler{
+func NewCategoryHandler(categoryUsecase services.CategoryUsecase) *CategoryHandler {
 	return &CategoryHandler{
 		CategoryUsecase: categoryUsecase,
 	}
+}
+
+func (catH *CategoryHandler) AddCategory(c *gin.Context) {
+	cat := c.Query("category")
+	categoryRes, err := catH.CategoryUsecase.AddCategory(cat)
+	if err != nil {
+		errRes := response.ClientResponse(http.StatusBadRequest, "Couldn't add category", nil, err.Error())
+		c.JSON(http.StatusBadRequest, errRes)
+	}
+	successRes := response.ClientResponse(http.StatusOK, "Category added successfully", categoryRes, nil)
+	c.JSON(http.StatusOK, successRes)
 }
