f20ca18f0b6f19ef747a1c429d69740098a7ff24
Anandhu4456 <anandhurajan56@gmail.com>
2023-11-22 17:45:15 +0530

    created a constructor function in wishlist handler

--- pkg/api/handlers/wishlist.go
+++ pkg/api/handlers/wishlist.go
@@ -1,1 +1,16 @@
 package handlers
+
+import (
+	services"github.com/Anandhu4456/go-Ecommerce/pkg/usecase/interfaces"
+)
+
+type WishlistHandler struct{
+	wishlistUsecase services.WishlistUsecase
+}
+
+// Constructor function 
+func NewWishlistHandler(wishlistUsecase services.WishlistUsecase)*WishlistHandler{
+	return &WishlistHandler{
+		wishlistUsecase: wishlistUsecase,
+	}
+}
