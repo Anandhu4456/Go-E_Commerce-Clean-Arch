327692be45536671fd9dab2c2090e5af5e306335
Anandhu4456 <anandhurajan56@gmail.com>
2023-11-22 12:52:05 +0530

    created a constructor function in coupon handler

--- pkg/api/handlers/inventory.go
+++ pkg/api/handlers/inventory.go
@@ -1,1 +1,15 @@
 package handlers
+
+import (
+	services"github.com/Anandhu4456/go-Ecommerce/pkg/usecase/interfaces"
+)
+
+type CouponHandler struct{
+	couponUsecase services.CouponUsecase
+}
+// Constructor function
+func NewCouponHandler(couponUsecase services.CouponUsecase)*CouponHandler{
+	return &CouponHandler{
+		couponUsecase: couponUsecase,
+	}
+}
