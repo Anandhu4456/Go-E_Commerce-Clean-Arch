c7d8bda3b24e7fd69e67c40ab3151b66319fd1fb
Anandhu4456 <anandhurajan56@gmail.com>
2023-11-22 14:17:34 +0530

    created createnewcoupon function in coupon handler

--- pkg/api/handlers/inventory.go
+++ pkg/api/handlers/inventory.go
@@ -1,15 +1,38 @@
 package handlers
 
 import (
-	services"github.com/Anandhu4456/go-Ecommerce/pkg/usecase/interfaces"
+	"net/http"
+
+	"github.com/Anandhu4456/go-Ecommerce/pkg/domain"
+	services "github.com/Anandhu4456/go-Ecommerce/pkg/usecase/interfaces"
+	"github.com/Anandhu4456/go-Ecommerce/pkg/utils/response"
+	"github.com/gin-gonic/gin"
 )
 
-type CouponHandler struct{
+type CouponHandler struct {
 	couponUsecase services.CouponUsecase
 }
+
 // Constructor function
-func NewCouponHandler(couponUsecase services.CouponUsecase)*CouponHandler{
+func NewCouponHandler(couponUsecase services.CouponUsecase) *CouponHandler {
 	return &CouponHandler{
 		couponUsecase: couponUsecase,
 	}
+}
+
+func (coupH *CouponHandler) CreateNewCoupon(c *gin.Context) {
+	var coupon domain.Coupon
+
+	if err := c.BindJSON(&coupon); err != nil {
+		errorRes := response.ClientResponse(http.StatusBadRequest, "fields provided are in wrong format", nil, err.Error())
+		c.JSON(http.StatusBadRequest, errorRes)
+		return
+	}
+	if err := coupH.couponUsecase.Addcoupon(coupon); err != nil {
+		errorRes := response.ClientResponse(http.StatusBadRequest, "Could not add the Coupon", nil, err.Error())
+		c.JSON(http.StatusBadRequest, errorRes)
+		return
+	}
+	successRes := response.ClientResponse(http.StatusOK, "Successfully added the coupon", nil, nil)
+	c.JSON(http.StatusOK, successRes)
 }
