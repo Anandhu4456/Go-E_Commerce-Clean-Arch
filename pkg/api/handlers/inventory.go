55f77889dbb70eef3c4e393788e0587c674a2e1c
Anandhu4456 <anandhurajan56@gmail.com>
2023-11-24 10:37:44 +0530

    created a constructor function in inventory handler

--- pkg/api/handlers/inventory.go
+++ pkg/api/handlers/inventory.go
@@ -1,1 +1,17 @@
 package handlers
+
+import (
+	services "github.com/Anandhu4456/go-Ecommerce/pkg/usecase/interfaces"
+)
+
+type InventoryHandler struct {
+	inventoryUsecase services.InventoryUsecase
+}
+
+// Constructor funciton
+
+func NewInventoryHandler(inventoryUsecase services.InventoryUsecase) *InventoryHandler {
+	return &InventoryHandler{
+		inventoryUsecase: inventoryUsecase,
+	}
+}
