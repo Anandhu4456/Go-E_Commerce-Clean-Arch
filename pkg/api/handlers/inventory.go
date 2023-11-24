0707d6e874a40e61fffb4317b4d2835b6337ab24
Anandhu4456 <anandhurajan56@gmail.com>
2023-11-24 11:11:52 +0530

    created add inventory function in inventory handler

--- pkg/api/handlers/inventory.go
+++ pkg/api/handlers/inventory.go
@@ -1,7 +1,13 @@
 package handlers
 
 import (
+	"net/http"
+	"strconv"
+
 	services "github.com/Anandhu4456/go-Ecommerce/pkg/usecase/interfaces"
+	"github.com/Anandhu4456/go-Ecommerce/pkg/utils/models"
+	"github.com/Anandhu4456/go-Ecommerce/pkg/utils/response"
+	"github.com/gin-gonic/gin"
 )
 
 type InventoryHandler struct {
@@ -14,4 +20,54 @@ func NewInventoryHandler(inventoryUsecase services.InventoryUsecase) *InventoryH
 	return &InventoryHandler{
 		inventoryUsecase: inventoryUsecase,
 	}
+}
+
+func (invH *InventoryHandler) AddInventory(c *gin.Context) {
+	var inventory models.Inventory
+
+	categoryId, err := strconv.Atoi(c.Request.FormValue("category_id"))
+	if err != nil {
+		errRes := response.ClientResponse(http.StatusBadRequest, "form file error", nil, err.Error())
+		c.JSON(http.StatusBadRequest, errRes)
+		return
+	}
+	productName := c.Request.FormValue("product_name")
+	description := c.Request.FormValue("description")
+
+	p, err := strconv.Atoi(c.Request.FormValue("price"))
+	if err != nil {
+		errRes := response.ClientResponse(http.StatusBadRequest, "form file error", nil, err.Error())
+		c.JSON(http.StatusBadRequest, errRes)
+		return
+	}
+	price := float64(p)
+	stock, err := strconv.Atoi(c.Request.FormValue("stock"))
+	if err != nil {
+		errRes := response.ClientResponse(http.StatusBadRequest, "form file error", nil, err.Error())
+		c.JSON(http.StatusBadRequest, errRes)
+		return
+	}
+	image, err := c.FormFile("image")
+	if err != nil {
+		errRes := response.ClientResponse(http.StatusBadRequest, "retrieving image from form failedd", nil, err.Error())
+		c.JSON(http.StatusBadRequest, errRes)
+		return
+	}
+
+	inventory.CategoryID = categoryId
+	inventory.Description = description
+	inventory.ProductName = productName
+	inventory.Price = price
+	inventory.Stock = stock
+	// inventory.Image = image
+
+	InventoryResp, err := invH.inventoryUsecase.AddInventory(inventory, image)
+	if err != nil {
+		errRes := response.ClientResponse(http.StatusBadRequest, "couldn't add inventory", nil, err.Error())
+		c.JSON(http.StatusBadRequest, errRes)
+		return
+	}
+
+	successRes := response.ClientResponse(http.StatusOK, "successfully added inventory", InventoryResp, nil)
+	c.JSON(http.StatusOK, successRes)
 }
