e82ac2b219ea7cb773fdee270fc71e24709ac735
Anandhu4456 <anandhurajan56@gmail.com>
2023-11-27 10:49:54 +0530

    created a constructor function in order handler

--- pkg/api/handlers/order.go
+++ pkg/api/handlers/order.go
@@ -1,1 +1,17 @@
 package handlers
+
+import (
+	services"github.com/Anandhu4456/go-Ecommerce/pkg/usecase/interfaces"
+)
+
+type OrderHandler struct{
+	orderUsecase services.OrderUsecase
+}
+
+// Constructor function
+
+func NewOrderHandler(orderUsecase services.OrderUsecase)*OrderHandler{
+	return &OrderHandler{
+		orderUsecase: orderUsecase,
+	}
+}
