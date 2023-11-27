8551a52b725d949bfe2d29a1a67ed36a8ec8e6c0
Anandhu4456 <anandhurajan56@gmail.com>
2023-11-27 11:43:05 +0530

    created get orders function in order handler

--- pkg/api/handlers/order.go
+++ pkg/api/handlers/order.go
@@ -1,17 +1,56 @@
 package handlers
 
 import (
-	services"github.com/Anandhu4456/go-Ecommerce/pkg/usecase/interfaces"
+	"net/http"
+	"strconv"
+
+	"github.com/Anandhu4456/go-Ecommerce/pkg/helper"
+	services "github.com/Anandhu4456/go-Ecommerce/pkg/usecase/interfaces"
+	"github.com/Anandhu4456/go-Ecommerce/pkg/utils/response"
+	"github.com/gin-gonic/gin"
 )
 
-type OrderHandler struct{
+type OrderHandler struct {
 	orderUsecase services.OrderUsecase
 }
 
 // Constructor function
 
-func NewOrderHandler(orderUsecase services.OrderUsecase)*OrderHandler{
+func NewOrderHandler(orderUsecase services.OrderUsecase) *OrderHandler {
 	return &OrderHandler{
 		orderUsecase: orderUsecase,
 	}
+}
+
+func (orH *OrderHandler) GetOrders(c *gin.Context) {
+	pageStr := c.Query("page")
+	page, err := strconv.Atoi(pageStr)
+	if err != nil {
+		errRes := response.ClientResponse(http.StatusBadRequest, "page number not in right format", nil, err.Error())
+		c.JSON(http.StatusBadRequest, errRes)
+		return
+	}
+
+	limitStr := c.Query("limit")
+	limit, err := strconv.Atoi(limitStr)
+	if err != nil {
+		errRes := response.ClientResponse(http.StatusBadRequest, "limit number not in right format", nil, err.Error())
+		c.JSON(http.StatusBadRequest, errRes)
+		return
+	}
+	id, err := helper.GetUserId(c)
+	if err != nil {
+		errRes := response.ClientResponse(http.StatusBadRequest, "geting user id failed", nil, err.Error())
+		c.JSON(http.StatusBadRequest, errRes)
+		return
+	}
+	orders, err := orH.orderUsecase.GetOrders(id, page, limit)
+	if err != nil {
+		errRes := response.ClientResponse(http.StatusBadRequest, "couldn't retrieve records", nil, err.Error())
+		c.JSON(http.StatusBadRequest, errRes)
+		return
+	}
+
+	successRes := response.ClientResponse(http.StatusOK, "successfully got orders", orders, nil)
+	c.JSON(http.StatusOK, successRes)
 }
