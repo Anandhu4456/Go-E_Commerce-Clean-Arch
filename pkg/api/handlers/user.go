b62da819baf3b148d5d1205813ccd3d89c98a44c
Anandhu4456 <anandhurajan56@gmail.com>
2023-11-28 15:42:07 +0530

    created change password function in userhandler

--- pkg/api/handlers/user.go
+++ pkg/api/handlers/user.go
@@ -43,3 +43,27 @@ func (uH *UserHandler) AddAddress(c *gin.Context) {
 	successRes := response.ClientResponse(http.StatusOK, "successfully added address", nil, nil)
 	c.JSON(http.StatusOK, successRes)
 }
+
+func (uH *UserHandler) ChangePassword(c *gin.Context) {
+	userId, err := helper.GetUserId(c)
+	if err != nil {
+		errRes := response.ClientResponse(http.StatusBadRequest, "couldn't find user id", nil, err.Error())
+		c.JSON(http.StatusBadRequest, errRes)
+		return
+	}
+	var changePass models.ChangePassword
+
+	if err := c.BindJSON(&changePass); err != nil {
+		errRes := response.ClientResponse(http.StatusBadRequest, "fields provided are in wrong format", nil, err.Error())
+		c.JSON(http.StatusBadRequest, errRes)
+		return
+	}
+	if err := uH.userusecase.ChangePassword(userId, changePass.OldPassword, changePass.NewPassword, changePass.RePassword); err != nil {
+		errRes := response.ClientResponse(http.StatusBadRequest, "couldn't change the password", nil, err.Error())
+		c.JSON(http.StatusBadRequest, errRes)
+		return
+	}
+
+	successRes := response.ClientResponse(http.StatusOK, "password changed successfully", nil, nil)
+	c.JSON(http.StatusOK, successRes)
+}
