e39b6fe2bc65bc45856613b4bdf7857ce272f888
Anandhu4456 <anandhurajan56@gmail.com>
2023-11-28 12:15:13 +0530

    created a constructor function in payment handler

--- pkg/api/handlers/payment.go
+++ pkg/api/handlers/payment.go
@@ -1,1 +1,17 @@
 package handlers
+
+import (
+	services"github.com/Anandhu4456/go-Ecommerce/pkg/usecase/interfaces"
+)
+
+type PaymentHandler struct{
+	paymentUsecase services.PaymentUsecase
+}
+
+// Constructor function
+
+func NewPaymentHandler(payUsecase services.PaymentUsecase)*PaymentHandler{
+	return &PaymentHandler{
+		paymentUsecase: payUsecase,
+	}
+}
