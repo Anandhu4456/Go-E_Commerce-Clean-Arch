b7fe6b0664cd43e326395b5dba906234eb185e24
Anandhu4456 <anandhurajan56@gmail.com>
2023-11-28 14:32:41 +0530

    created a constructor function in user handler

--- pkg/api/handlers/user.go
+++ pkg/api/handlers/user.go
@@ -1,1 +1,16 @@
 package handlers
+
+import (
+	services "github.com/Anandhu4456/go-Ecommerce/pkg/usecase/interfaces"
+)
+
+type UserHandler struct {
+	userusecase services.UserUsecase
+}
+
+// Constructor function
+func NewUserHandler(userUsecase services.UserUsecase) *UserHandler {
+	return &UserHandler{
+		userusecase: userUsecase,
+	}
+}
