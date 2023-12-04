bdb3bb87fe9e1ea59fe073e8a110915dfc236cb3
Anandhu4456 <anandhurajan56@gmail.com>
2023-12-04 12:01:24 +0530

    setted user management routes for admin in routes for admin

--- pkg/routes/admin.go
+++ pkg/routes/admin.go
@@ -1,1 +1,19 @@
 package routes
+
+import (
+	"github.com/Anandhu4456/go-Ecommerce/pkg/api/handlers"
+	"github.com/Anandhu4456/go-Ecommerce/pkg/api/middleware"
+	"github.com/gin-gonic/gin"
+)
+
+func AdminRoutes(engine *gin.RouterGroup,adminHandler *handlers.AdminHandler /*userHandler *handlers.UserHandler,*/,categoryHandler *handlers.CategoryHandler,inventoryHandler *handlers.InventoryHandler,orderHandler *handlers.OrderHandler,paymentHandler *handlers.PaymentHandler,offerHandler *handlers.OfferHandler,couponHandler *handlers.CouponHandler ){
+	engine.POST("/adminlogin",adminHandler.LoginHandler)
+	engine.Use(middleware.AdminAuthMiddleware)
+
+	userManagement:=engine.Group("/users")
+	{
+		userManagement.POST("/block",adminHandler.BlockUser)
+		userManagement.POST("/unblock",adminHandler.UnblockUser)
+		userManagement.GET("/getusers",adminHandler.GetUsers)
+	}
+}
