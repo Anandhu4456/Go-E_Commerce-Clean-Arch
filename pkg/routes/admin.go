74424a47b75aed41c2a3c01c9d191dc281d93a3e
Anandhu4456 <anandhurajan56@gmail.com>
2023-12-04 13:43:13 +0530

    added inventorymanagement routes in admin

--- pkg/routes/admin.go
+++ pkg/routes/admin.go
@@ -24,4 +24,17 @@ func AdminRoutes(engine *gin.RouterGroup, adminHandler *handlers.AdminHandler /*
 		categoryManagement.PATCH("/update", categoryHandler.UpdateCategory)
 		categoryManagement.DELETE("/delete", categoryHandler.DeleteCategory)
 	}
+
+	inventoryManagement := engine.Group("/inventories")
+	{
+		inventoryManagement.GET("", inventoryHandler.ListProdutcs)
+		inventoryManagement.GET("/details", inventoryHandler.ShowIndividualProducts)
+		inventoryManagement.POST("/add", inventoryHandler.AddInventory)
+		inventoryManagement.POST("/add-image", inventoryHandler.AddImage)
+		inventoryManagement.PATCH("/update", inventoryHandler.UpdateInventory)
+		inventoryManagement.PATCH("/update-image", inventoryHandler.UpdateImage)
+		inventoryManagement.DELETE("/delete-image", inventoryHandler.DeleteImage)
+		inventoryManagement.DELETE("/delete", inventoryHandler.DeleteInventory)
+	}
+
 }
