99005314c4fc78246d1d128288e68fabf08c4236
Anandhu4456 <anandhurajan56@gmail.com>
2023-12-04 11:35:42 +0530

    initialized a server in api directory with help of gin

--- pkg/api/server.go
+++ pkg/api/server.go
@@ -1,1 +1,22 @@
 package http
+
+import (
+	handlers"github.com/Anandhu4456/go-Ecommerce/pkg/api/handlers"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	swaggerfiles "github.com/swaggo/files"
+	"github.com/gin-gonic/gin"
+)
+
+// http server for the web application
+type ServerHTTP struct{
+	engine *gin.Engine
+}
+
+// Constructor function
+
+func NewServerHTTP(categoryHandler *handlers.CategoryHandler,inventoryHandler *handlers.InventoryHandler,userHandler *handlers.UserHandler,adminHandler *handlers.AdminHandler,otpHandler *handlers.OtpHandler,cartHandler *handlers.CartHandler,orderHandler *handlers.OrderHandler,paymentHandler *handlers.PaymentHandler,wishlistHandler *handlers.WishlistHandler,offerHandler *handlers.OfferHandler,couponHandler *handlers.CouponHandler)*ServerHTTP{
+	engine:=gin.New()
+	engine.Use(gin.Logger())
+	engine.LoadHTMLGlob("pkg/templates/*.html")
+	engine.GET("/swagger/*any",ginSwagger.WrapHandler(swaggerfiles.Handler))
+}
