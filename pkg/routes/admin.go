81afb3d31a1acd2e8fdec7879c2e3d2c91395aac
Anandhu4456 <anandhurajan56@gmail.com>
2023-12-04 13:27:26 +0530

    added categorymanagement routes in admin

--- pkg/routes/admin.go
+++ pkg/routes/admin.go
@@ -6,14 +6,22 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func AdminRoutes(engine *gin.RouterGroup,adminHandler *handlers.AdminHandler /*userHandler *handlers.UserHandler,*/,categoryHandler *handlers.CategoryHandler,inventoryHandler *handlers.InventoryHandler,orderHandler *handlers.OrderHandler,paymentHandler *handlers.PaymentHandler,offerHandler *handlers.OfferHandler,couponHandler *handlers.CouponHandler ){
-	engine.POST("/adminlogin",adminHandler.LoginHandler)
+func AdminRoutes(engine *gin.RouterGroup, adminHandler *handlers.AdminHandler /*userHandler *handlers.UserHandler,*/, categoryHandler *handlers.CategoryHandler, inventoryHandler *handlers.InventoryHandler, orderHandler *handlers.OrderHandler, paymentHandler *handlers.PaymentHandler, offerHandler *handlers.OfferHandler, couponHandler *handlers.CouponHandler) {
+	engine.POST("/adminlogin", adminHandler.LoginHandler)
 	engine.Use(middleware.AdminAuthMiddleware)
 
-	userManagement:=engine.Group("/users")
+	userManagement := engine.Group("/users")
 	{
-		userManagement.POST("/block",adminHandler.BlockUser)
-		userManagement.POST("/unblock",adminHandler.UnblockUser)
-		userManagement.GET("/getusers",adminHandler.GetUsers)
+		userManagement.POST("/block", adminHandler.BlockUser)
+		userManagement.POST("/unblock", adminHandler.UnblockUser)
+		userManagement.GET("/getusers", adminHandler.GetUsers)
+	}
+
+	categoryManagement := engine.Group("/category")
+	{
+		categoryManagement.GET("/categories", categoryHandler.Categories)
+		categoryManagement.POST("/add", categoryHandler.AddCategory)
+		categoryManagement.PATCH("/update", categoryHandler.UpdateCategory)
+		categoryManagement.DELETE("/delete", categoryHandler.DeleteCategory)
 	}
 }
