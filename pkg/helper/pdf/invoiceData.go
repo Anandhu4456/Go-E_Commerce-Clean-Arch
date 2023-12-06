967ffcdcdc48333be2acdfffa9d72dc71d1eaf19
Anandhu4456 <anandhurajan56@gmail.com>
2023-12-06 14:51:41 +0530

    created return item total amount function in pdf invoice data

--- pkg/helper/pdf/invoiceData.go
+++ pkg/helper/pdf/invoiceData.go
@@ -1,13 +1,19 @@
 package pdf
 
-type InvoiceData struct{
-	Title string
-	Quantity int
-	Price int
+type InvoiceData struct {
+	Title       string
+	Quantity    int
+	Price       int
 	TotalAmount int
 }
 
-func (d *InvoiceData)CalculateTotalAmount()int{
-	totalAmount:=d.Quantity *d.Price
+func (d *InvoiceData) CalculateTotalAmount() int {
+	totalAmount := d.Quantity * d.Price
 	return totalAmount
+}
+
+func (d *InvoiceData) ReturnItemTotalAmount() float64 {
+	totalAmount := d.CalculateTotalAmount()
+	converted := float64(totalAmount) / 100
+	return converted
 }
