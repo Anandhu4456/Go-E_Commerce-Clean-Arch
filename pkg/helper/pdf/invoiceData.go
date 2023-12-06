b4b2d19054df2c913df1e0c2d10c90d6ea3bf31d
Anandhu4456 <anandhurajan56@gmail.com>
2023-12-06 15:02:38 +0530

    created new invoice data function in helper pdf invoice data

--- pkg/helper/pdf/invoiceData.go
+++ pkg/helper/pdf/invoiceData.go
@@ -1,5 +1,7 @@
 package pdf
 
+import "errors"
+
 type InvoiceData struct {
 	Title       string
 	Quantity    int
@@ -16,4 +18,29 @@ func (d *InvoiceData) ReturnItemTotalAmount() float64 {
 	totalAmount := d.CalculateTotalAmount()
 	converted := float64(totalAmount) / 100
 	return converted
+}
+
+func (d *InvoiceData) ReturnItemPrice() float64 {
+	returnPrice := float64(d.Price) / 100
+	return returnPrice
+}
+
+func NewInvoiceData(title string, qty int, price interface{}) (*InvoiceData, error) {
+	var convertedPrice int
+
+	switch priceValue := price.(type) {
+	case int:
+		convertedPrice = priceValue * 100
+	case float32:
+		convertedPrice = int(priceValue) * 100
+	case float64:
+		convertedPrice = int(priceValue) * 100
+	default:
+		return nil, errors.New("type not permitted")
+	}
+	return &InvoiceData{
+		Title:    title,
+		Quantity: qty,
+		Price:    convertedPrice,
+	}, nil
 }
