8a4e604e65dd05f72ac4628033f0b1319d45326b
Anandhu4456 <anandhurajan56@gmail.com>
2023-12-06 14:43:55 +0530

    created calculate invoice total amount  function in helper pdf

--- pkg/helper/pdf/invoice.go
+++ pkg/helper/pdf/invoice.go
@@ -1,1 +1,25 @@
 package pdf
+
+type Invoice struct {
+	Name        string
+	Address     string
+	InvoiceItem []*InvoiceData
+}
+
+func CreateInvoice(name string, address string, invoiceItem []*InvoiceData) *Invoice {
+	return &Invoice{
+		Name:        name,
+		Address:     address,
+		InvoiceItem: invoiceItem,
+	}
+}
+
+func (i *Invoice) CalculateInvoiceTotalAmount() float64 {
+	var invoiceTotalAmount int = 0
+	for _, data := range i.InvoiceItem {
+		amount := data.CalculateTotalAmount()
+		invoiceTotalAmount += amount
+	}
+	totalAmount := float64(invoiceTotalAmount) / 100
+	return totalAmount
+}
