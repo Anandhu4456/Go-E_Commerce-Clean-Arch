2b4589d19a5c5aae877044b0af01658b51b1d573
Anandhu4456 <anandhurajan56@gmail.com>
2023-12-06 14:48:26 +0530

    created calculatetotal amount function in pdf invoice data

--- pkg/helper/pdf/invoiceData.go
+++ pkg/helper/pdf/invoiceData.go
@@ -1,1 +1,13 @@
 package pdf
+
+type InvoiceData struct{
+	Title string
+	Quantity int
+	Price int
+	TotalAmount int
+}
+
+func (d *InvoiceData)CalculateTotalAmount()int{
+	totalAmount:=d.Quantity *d.Price
+	return totalAmount
+}
