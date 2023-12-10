63743ac56063e28a2128d281dbb1b837141cb27d
Anandhu4456 <anandhurajan56@gmail.com>
2023-12-10 14:57:22 +0530

    added swagger annotations in inventory handler

--- pkg/api/handlers/inventory.go
+++ pkg/api/handlers/inventory.go
@@ -22,6 +22,21 @@ func NewInventoryHandler(inventoryUsecase services.InventoryUsecase) *InventoryH
 	}
 }
 
+// @Summary		Add Inventory
+// @Description	Admin can add new  products
+// @Tags			Admin
+// @Accept			multipart/form-data
+// @Produce		    json
+// @Param			category_id		formData	string	true	"category_id"
+// @Param			product_name	formData	string	true	"product_name"
+// @Param			description		formData	string	true	"description"
+// @Param			price	formData	string	true	"price"
+// @Param			stock		formData	string	true	"stock"
+// @Param           image      formData     file   true   "image"
+// @Security		Bearer
+// @Success		200	{object}	response.Response{}
+// @Failure		500	{object}	response.Response{}
+// @Router			/admin/inventories/add [post]
 func (invH *InventoryHandler) AddInventory(c *gin.Context) {
 	var inventory models.Inventory
 
@@ -72,6 +87,17 @@ func (invH *InventoryHandler) AddInventory(c *gin.Context) {
 	c.JSON(http.StatusOK, successRes)
 }
 
+// @Summary		Add image to an Inventory
+// @Description	Admin can add new image to product
+// @Tags			Admin
+// @Accept			multipart/form-data
+// @Produce		    json
+// @Param			product_id	formData	string	true	"product_id"
+// @Param           image      formData     file   true   "image"
+// @Security		Bearer
+// @Success		200	{object}	response.Response{}
+// @Failure		500	{object}	response.Response{}
+// @Router			/admin/inventories/add-image [post]
 func (invH *InventoryHandler) AddImage(c *gin.Context) {
 	productId, err := strconv.Atoi(c.Request.FormValue("product_id"))
 	if err != nil {
@@ -96,6 +122,17 @@ func (invH *InventoryHandler) AddImage(c *gin.Context) {
 	c.JSON(http.StatusOK, successRes)
 }
 
+// @Summary		Delete Inventory image
+// @Description	Admin can delete a product image
+// @Tags			Admin
+// @Accept			json
+// @Produce		    json
+// @Param			product_id	query	string	true	"product_id"
+// @Param			image_id	query	string	true	"image_id"
+// @Security		Bearer
+// @Success		200	{object}	response.Response{}
+// @Failure		500	{object}	response.Response{}
+// @Router			/admin/inventories/delete-image [delete]
 func (invH *InventoryHandler) DeleteImage(c *gin.Context) {
 	productId, err := strconv.Atoi(c.Query("product_id"))
 	if err != nil {
@@ -120,6 +157,17 @@ func (invH *InventoryHandler) DeleteImage(c *gin.Context) {
 	c.JSON(http.StatusOK, successRes)
 }
 
+// @Summary		Update inventory
+// @Description	Admin can update inventory details
+// @Tags			Admin
+// @Accept			json
+// @Produce		    json
+// @Param			id	query	string	true	"id"	
+// @Param			updateinventory	body	models.UpdateInventory	true	"Update Inventory"
+// @Security		Bearer
+// @Success		200	{object}	response.Response{}
+// @Failure		500	{object}	response.Response{}
+// @Router			/admin/inventories/update [patch]
 func (invH *InventoryHandler) UpdateInventory(c *gin.Context) {
 	invIdStr := c.Query("id")
 	invId, err := strconv.Atoi(invIdStr)
@@ -147,6 +195,17 @@ func (invH *InventoryHandler) UpdateInventory(c *gin.Context) {
 
 }
 
+// @Summary		Update image
+// @Description	Admin can update image of the inventory
+// @Tags			Admin
+// @Accept			multipart/form-data
+// @Produce		    json
+// @Param			id	query	string	true	"id"	
+// @Param           image      formData     file   true   "image"
+// @Security		Bearer
+// @Success		200	{object}	response.Response{}
+// @Failure		500	{object}	response.Response{}
+// @Router			/admin/inventories/update-image [patch]
 func (invH *InventoryHandler) UpdateImage(c *gin.Context) {
 	invIdStr := c.Query("inventory_id")
 	invId, err := strconv.Atoi(invIdStr)
@@ -172,6 +231,16 @@ func (invH *InventoryHandler) UpdateImage(c *gin.Context) {
 	c.JSON(http.StatusOK, successRes)
 }
 
+// @Summary		Delete Inventory
+// @Description	Admin can delete a product
+// @Tags			Admin
+// @Accept			json
+// @Produce		    json
+// @Param			id	query	string	true	"id"
+// @Security		Bearer
+// @Success		200	{object}	response.Response{}
+// @Failure		500	{object}	response.Response{}
+// @Router			/admin/inventories/delete [delete]
 func (invH *InventoryHandler) DeleteInventory(c *gin.Context) {
 	invId := c.Query("id")
 	if err := invH.inventoryUsecase.DeleteInventory(invId); err != nil {
@@ -184,6 +253,16 @@ func (invH *InventoryHandler) DeleteInventory(c *gin.Context) {
 	c.JSON(http.StatusOK, successRes)
 }
 
+// @Summary		Show Product Details
+// @Description	client can view the details of the product
+// @Tags			Products
+// @Accept			json
+// @Produce		    json
+// @Param			inventoryID	query	string	true	"Inventory ID"
+// @Security		Bearer
+// @Success		200	{object}	response.Response{}
+// @Failure		500	{object}	response.Response{}
+// @Router			/products/details [get]
 func (invH *InventoryHandler) ShowIndividualProducts(c *gin.Context) {
 	id := c.Query("inventory_id")
 	products, err := invH.inventoryUsecase.ShowIndividualProducts(id)
@@ -197,6 +276,17 @@ func (invH *InventoryHandler) ShowIndividualProducts(c *gin.Context) {
 	c.JSON(http.StatusOK, successRes)
 }
 
+// @Summary		List Products
+// @Description	client can view the list of available products
+// @Tags			Products
+// @Accept			json
+// @Produce		    json
+// @Param			page	query  string 	true	"page"
+// @Param			limit	query  string 	true	"limit"
+// @Security		Bearer
+// @Success		200	{object}	response.Response{}
+// @Failure		500	{object}	response.Response{}
+// @Router			/products [get]
 func (invH *InventoryHandler) ListProdutcs(c *gin.Context) {
 	pageStr := c.Query("page")
 	page, err := strconv.Atoi(pageStr)
@@ -223,6 +313,17 @@ func (invH *InventoryHandler) ListProdutcs(c *gin.Context) {
 	c.JSON(http.StatusOK, successRes)
 }
 
+// @Summary		Search Products
+// @Description	client can search with a key and get the list of  products similar to that key
+// @Tags			Products
+// @Accept			json
+// @Produce		    json
+// @Param			page	query  string 	true	"page"
+// @Param			limit	query  string 	true	"limit"
+// @Param			searchkey 	query  string 	true	"searchkey"
+// @Success		200	{object}	response.Response{}
+// @Failure		500	{object}	response.Response{}
+// @Router			/products/search [get]
 func (invH *InventoryHandler) SearchProducts(c *gin.Context) {
 	pageStr := c.Query("page")
 	page, err := strconv.Atoi(pageStr)
@@ -251,6 +352,17 @@ func (invH *InventoryHandler) SearchProducts(c *gin.Context) {
 	c.JSON(http.StatusOK, successRes)
 }
 
+// @Summary		List Products
+// @Description	client can view the list of available products
+// @Tags			Admin
+// @Accept			json
+// @Produce		    json
+// @Param			page	query  string 	true	"page"
+// @Param			limit	query  string 	true	"limit"
+// @Security		Bearer
+// @Success		200	{object}	response.Response{}
+// @Failure		500	{object}	response.Response{}
+// @Router			/admin/products [get]
 func (invH *InventoryHandler) AdminListProdutcs(c *gin.Context) {
 	pageStr := c.Query("page")
 	page, err := strconv.Atoi(pageStr)
@@ -277,6 +389,17 @@ func (invH *InventoryHandler) AdminListProdutcs(c *gin.Context) {
 	c.JSON(http.StatusOK, successRes)
 }
 
+// @Summary		filter Products by category
+// @Description	client can filter with a category and get the list of  products in the category
+// @Tags			Products
+// @Accept			json
+// @Produce		    json
+// @Param			page	query  string 	true	"page"
+// @Param			limit	query  string 	true	"limit"
+// @Param			catID 	query  string 	true	"category ID"
+// @Success		200	{object}	response.Response{}
+// @Failure		500	{object}	response.Response{}
+// @Router			/products/category [get]
 func (invH *InventoryHandler) GetCategoryProducts(c *gin.Context) {
 	pageStr := c.Query("page")
 	page, err := strconv.Atoi(pageStr)
