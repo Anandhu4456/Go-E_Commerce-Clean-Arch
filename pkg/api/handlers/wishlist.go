bfbdf5c205c1b4404435749f2845a2b00f836d7d
Anandhu4456 <anandhurajan56@gmail.com>
2023-12-11 11:39:23 +0530

    added swagger annotations in wishlist handler

--- pkg/api/handlers/wishlist.go
+++ pkg/api/handlers/wishlist.go
@@ -21,6 +21,16 @@ func NewWishlistHandler(wishlistUsecase services.WishlistUsecase) *WishlistHandl
 	}
 }
 
+// @Summary		Add To Wishlist
+// @Description	Add products to Wishlsit  for the purchase
+// @Tags			User
+// @Accept			json
+// @Produce		json
+// @Param			inventory	query	string	true	"inventory ID"
+// @Security		Bearer
+// @Success		200	{object}	response.Response{}
+// @Failure		500	{object}	response.Response{}
+// @Router			/users/home/add-to-wishlist [post]
 func (wiH *WishlistHandler) AddtoWishlist(c *gin.Context) {
 	userId, err := helper.GetUserId(c)
 	if err != nil {
@@ -44,6 +54,15 @@ func (wiH *WishlistHandler) AddtoWishlist(c *gin.Context) {
 	c.JSON(http.StatusOK, successRes)
 }
 
+// @Summary		Remove from Wishlist
+// @Description	user can remove products from their wishlist
+// @Tags			User
+// @Produce		    json
+// @Param			inventory	query	string	true	"inventory id"
+// @Security		Bearer
+// @Success		200	{object}	response.Response{}
+// @Failure		500	{object}	response.Response{}
+// @Router			/users/wishlist/remove [delete]
 func (wiH *WishlistHandler) RemoveFromWishlist(c *gin.Context) {
 	id, err := helper.GetUserId(c)
 	if err != nil {
@@ -73,6 +92,14 @@ func (wiH *WishlistHandler) RemoveFromWishlist(c *gin.Context) {
 	c.JSON(http.StatusOK, successRes)
 }
 
+// @Summary		Get Wishlist
+// @Description	user can view their wishlist details
+// @Tags			User
+// @Produce		    json
+// @Security		Bearer
+// @Success		200	{object}	response.Response{}
+// @Failure		500	{object}	response.Response{}
+// @Router			/users/wishlist [get]
 func (wiH *WishlistHandler) GetWishlist(c *gin.Context) {
 	id, err := helper.GetUserId(c)
 	if err != nil {
