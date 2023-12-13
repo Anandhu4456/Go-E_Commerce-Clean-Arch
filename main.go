a195530d7a4cd7354058b923bc08b622ab6415c6
Anandhu4456 <anandhurajan56@gmail.com>
2023-12-13 12:01:18 +0530

    added git ignore file

--- main.go
+++ main.go
@@ -4,18 +4,18 @@ import (
 	"log"
 
 	"github.com/Anandhu4456/go-Ecommerce/pkg/config"
-	"github.com/Anandhu4456/go-Ecommerce/pkg/di"
+	di "github.com/Anandhu4456/go-Ecommerce/pkg/di"
 )
 
-func main(){
-	config,configErr:=config.LoadConfig()
-	if configErr!=nil{
-		log.Fatal("cannot load config",configErr)
+func main() {
+	config, configErr := config.LoadConfig()
+	if configErr != nil {
+		log.Fatal("cannot load config", configErr)
 	}
-	server,err:=di.InitializeAPI(config)
-	if err!=nil{
-		log.Fatal("couldn't start server",err)
-	}else{
+	server, err := di.InitializeAPI(config)
+	if err != nil {
+		log.Fatal("couldn't start server", err)
+	} else {
 		server.Start()
 	}
 }
