a1a263702f5783e13e0dda64ca388c7ce9d6f7de
Anandhu4456 <anandhurajan56@gmail.com>
2023-12-13 11:45:00 +0530

    added env file and completed main function

--- docs/docs.go
+++ docs/docs.go
@@ -27,8 +27,8 @@ var SwaggerInfo = &swag.Spec{
 	Description:      "",
 	InfoInstanceName: "swagger",
 	SwaggerTemplate:  docTemplate,
-	LeftDelim:        "{{",
-	RightDelim:       "}}",
+	// LeftDelim:        "{{",
+	// RightDelim:       "}}",
 }
 
 func init() {
